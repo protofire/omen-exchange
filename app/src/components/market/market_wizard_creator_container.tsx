f1fe7c4da7fba4c60e4d37cf05d0d0575f892090
Mariano Aguero <mariano.aguero@altoros.com>
2019-09-19 14:17:44 -0300

    Add logger and try catch on submit

--- app/src/components/market/market_wizard_creator_container.tsx
+++ app/src/components/market/market_wizard_creator_container.tsx
@@ -3,6 +3,7 @@ import React, { FC, useState } from 'react'
 import { useWeb3Context } from 'web3-react'
 import moment from 'moment'
 
+import { getLogger } from '../../util/logger'
 import { MarketWizardCreator, MarketData } from './market_wizard_creator'
 import {
   RealitioService,
@@ -11,6 +12,8 @@ import {
   MarketMakerService,
 } from '../../services'
 import { getContractAddress } from '../../util/addresses'
+
+const logger = getLogger('Market::MarketWizardCreatorContainer')
 
 export enum Status {
   Ready = 'Ready',
@@ -21,6 +24,7 @@ export enum Status {
   ApproveDAIForMarketMaker = 'Approve dai for market maker',
   InitialTradeInMarketMaker = 'initial trade in market maker',
   Done = 'Done',
+  Error = 'Error',
 }
 
 const MarketWizardCreatorContainer: FC = () => {
@@ -30,70 +34,75 @@ const MarketWizardCreatorContainer: FC = () => {
   const [marketMakerAddress, setMarketMakerAddress] = useState<string | null>(null)
 
   const handleSubmit = async (data: MarketData) => {
-    if (!context.networkId || !context.library) {
-      throw new Error('Network is not available')
+    try {
+      if (!context.networkId || !context.library) {
+        throw new Error('Network is not available')
+      }
+      if (!data.resolution) {
+        throw new Error('resolution time was not specified')
+      }
+
+      const networkId = context.networkId
+      const provider = context.library
+      const user = await provider.getSigner().getAddress()
+
+      const { question, resolution, funding } = data
+      const openingDateMoment = moment(resolution)
+
+      setStatus(Status.PostingQuestion)
+      const questionId = await RealitioService.askQuestion(
+        question,
+        openingDateMoment,
+        provider,
+        networkId,
+      )
+      setQuestionId(questionId)
+
+      setStatus(Status.PrepareCondition)
+      const conditionId = await ConditionalTokenService.prepareCondition(
+        questionId,
+        provider,
+        networkId,
+      )
+
+      // approve movement of DAI to MarketMakerFactory
+      setStatus(Status.ApprovingDAI)
+      const fundingWei = ethers.utils.bigNumberify(funding).mul(ethers.constants.WeiPerEther)
+      const daiAddress = getContractAddress(networkId, 'dai')
+      const marketMakerFactoryAddress = getContractAddress(networkId, 'marketMakerFactory')
+      const daiService = new ERC20Service(daiAddress)
+
+      const hasEnoughAlowance = await daiService.hasEnoughAllowance(
+        provider,
+        user,
+        marketMakerFactoryAddress,
+        fundingWei,
+      )
+      if (!hasEnoughAlowance) {
+        await daiService.approve(provider, marketMakerFactoryAddress, fundingWei)
+      }
+
+      setStatus(Status.CreateMarketMaker)
+      const marketMakerAddress = await MarketMakerService.createMarketMaker(
+        conditionId,
+        fundingWei,
+        provider,
+        networkId,
+      )
+      setMarketMakerAddress(marketMakerAddress)
+
+      setStatus(Status.ApproveDAIForMarketMaker)
+      await daiService.approveUnlimited(provider, marketMakerAddress)
+
+      setStatus(Status.InitialTradeInMarketMaker)
+      const marketMakerService = new MarketMakerService(marketMakerAddress)
+      await marketMakerService.trade(provider, ['1000000000', '0'])
+
+      setStatus(Status.Done)
+    } catch (err) {
+      setStatus(Status.Error)
+      logger.error(err.message)
     }
-    if (!data.resolution) {
-      throw new Error('resolution time was not specified')
-    }
-
-    const networkId = context.networkId
-    const provider = context.library
-    const user = await provider.getSigner().getAddress()
-
-    const { question, resolution, funding } = data
-    const openingDateMoment = moment(resolution)
-
-    setStatus(Status.PostingQuestion)
-    const questionId = await RealitioService.askQuestion(
-      question,
-      openingDateMoment,
-      provider,
-      networkId,
-    )
-    setQuestionId(questionId)
-
-    setStatus(Status.PrepareCondition)
-    const conditionId = await ConditionalTokenService.prepareCondition(
-      questionId,
-      provider,
-      networkId,
-    )
-
-    // approve movement of DAI to MarketMakerFactory
-    setStatus(Status.ApprovingDAI)
-    const fundingWei = ethers.utils.bigNumberify(funding).mul(ethers.constants.WeiPerEther)
-    const daiAddress = getContractAddress(networkId, 'dai')
-    const marketMakerFactoryAddress = getContractAddress(networkId, 'marketMakerFactory')
-    const daiService = new ERC20Service(daiAddress)
-
-    const hasEnoughAlowance = await daiService.hasEnoughAllowance(
-      provider,
-      user,
-      marketMakerFactoryAddress,
-      fundingWei,
-    )
-    if (!hasEnoughAlowance) {
-      await daiService.approve(provider, marketMakerFactoryAddress, fundingWei)
-    }
-
-    setStatus(Status.CreateMarketMaker)
-    const marketMakerAddress = await MarketMakerService.createMarketMaker(
-      conditionId,
-      fundingWei,
-      provider,
-      networkId,
-    )
-    setMarketMakerAddress(marketMakerAddress)
-
-    setStatus(Status.ApproveDAIForMarketMaker)
-    await daiService.approveUnlimited(provider, marketMakerAddress)
-
-    setStatus(Status.InitialTradeInMarketMaker)
-    const marketMakerService = new MarketMakerService(marketMakerAddress)
-    await marketMakerService.trade(provider, ['1000000000', '0'])
-
-    setStatus(Status.Done)
   }
 
   return (
