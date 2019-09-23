6a5ef3b6336a139e828f01b00be6b6bf2b794d6f
Franco Victorio <victorio.franco@gmail.com>
2019-09-23 15:43:23 -0300

    Perform trade to set initial probabilities

--- app/src/util/tools.ts
+++ app/src/util/tools.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from 'ethers/utils'
+
 export const truncateStringInTheMiddle = (
   str: string,
   strLength = 41,
@@ -11,4 +13,30 @@ export const truncateStringInTheMiddle = (
     )}`
   }
   return str
+}
+
+/**
+ * Returns the amount of outcome tokens that need to be bought to set the probabilities to `targetProbabilities`,
+ * assuming the current probabilities are uniform.
+ *
+ * @param targetProbabilities - An array of probabilities specified over 100. For example [75, 25].
+ * @param funding - The initial funding of the market maker, in wei.
+ */
+export const computeInitialTradeOutcomeTokens = (
+  targetProbabilities: number[],
+  funding: BigNumber,
+): BigNumber[] => {
+  const numOutcomes = targetProbabilities.length
+  const netTradeAmountForOutcomes = targetProbabilities.map(targetProbability => {
+    const logProbabilities =
+      Math.log(targetProbability / (100 / numOutcomes)) / Math.log(numOutcomes)
+    const logProbabilitiesBN = new BigNumber(Math.round(logProbabilities * 10000))
+    return funding.mul(logProbabilitiesBN).div(10000)
+  })
+
+  const cost = netTradeAmountForOutcomes.reduce((a, b) => (a.lt(b) ? a : b)).mul(-1)
+
+  const buyAmounts = netTradeAmountForOutcomes.map(x => x.add(cost))
+
+  return buyAmounts
 }
