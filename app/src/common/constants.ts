1d23b7e562a4919dd42785f278a0b75e51af7413
Mariano Aguero <mariano.aguero@altoros.com>
2019-09-24 16:42:25 -0300

    Add app url env var

--- app/src/common/constants.ts
+++ app/src/common/constants.ts
@@ -4,3 +4,4 @@ export const CONNECTOR: string = process.env.REACT_APP_CONNECTOR || 'MetaMask'
 // Must be 0.1
 export const FEE = ethers.utils.parseEther('0.1')
 export const LOGGER_ID: string = process.env.REACT_APP_LOGGER_ID || 'gnosis-conditional-exchange'
+export const APP_URL: string = process.env.REACT_APP_URL || 'http://localhost:3000/'
