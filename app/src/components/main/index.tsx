3523c1ca0e9542e2ca74a36dcdb69dc8cc9f749e
Franco Victorio <victorio.franco@gmail.com>
2019-09-27 10:20:49 -0300

    Use HashRouter

--- app/src/components/main/index.tsx
+++ app/src/components/main/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { useWeb3Context } from 'web3-react'
 
 import { Home, MarketWizardCreatorContainer, MarketViewContainer } from '../index'
