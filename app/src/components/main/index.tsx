0de0b05e65b4209ecdcabd2f8fc0b84a0d9180a4
Franco Victorio <victorio.franco@gmail.com>
2019-09-27 08:29:07 -0300

    Connect web3 automatically in market view

--- app/src/components/main/index.tsx
+++ app/src/components/main/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { useWeb3Context } from 'web3-react'
 
 import { Home, MarketWizardCreatorContainer, MarketViewContainer } from '../index'
+import { CONNECTOR } from '../../common/constants'
 import { Header } from '../common/header'
 import { ConnectedWeb3 } from '../../hooks/connectedWeb3'
 
 const RedirectToHome = () => <Redirect to="/" />
+
+const ConnectWeb3 = () => {
+  const context = useWeb3Context()
+
+  React.useEffect(() => {
+    context.setConnector(CONNECTOR)
+  }, [])
+
+  return <></>
+}
 
 export const Main: React.FC = () => {
   return (
@@ -22,6 +34,7 @@ export const Main: React.FC = () => {
           </ConnectedWeb3>
           <Route component={RedirectToHome} />
         </Switch>
+        <Route path="/view/:address" exact component={ConnectWeb3} />
       </Router>
     </div>
   )
