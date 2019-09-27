560d2dd53c315a6a67dfd188870719b2918eadd7
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-09-27 10:39:36 -0300

    (add) button styles

--- app/src/components/common/button/index.tsx
+++ app/src/components/common/button/index.tsx
@@ -1,18 +1,48 @@
 import React, { ReactNode, HTMLAttributes } from 'react'
+import styled from 'styled-components'
+
+const ButtonContainer = styled.button`
+  align-items: center;
+  background-color: ${props => props.theme.colors.primary};
+  border-radius: 2px;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  display: flex;
+  font-size: 19px;
+  font-weight: 500;
+  height: 38px;
+  justify-content: center;
+  outline: none;
+  padding: 0 25px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: background-color 0.15s ease-out;
+  white-space: nowrap;
+
+  &:hover {
+    /* something */
+  }
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+`
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode
-  type?: 'button' | 'submit' | 'reset' | undefined
   disabled?: boolean
   onClick?: (e?: any) => void
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
 export const Button = (props: Props) => {
   const { children, disabled = false, onClick, ...restProps } = props
 
   return (
-    <button disabled={disabled} onClick={onClick} {...restProps}>
+    <ButtonContainer disabled={disabled} onClick={onClick} {...restProps}>
       {children}
-    </button>
+    </ButtonContainer>
   )
 }
