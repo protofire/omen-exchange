98023db9e8abc9cea7840dc3c8a91ab02cd647c9
Mariano Aguero <mariano.aguero@altoros.com>
2019-10-09 16:59:06 -0300

    Add new contract createFixedProductMarketMaker

--- app/src/components/market/market_wizard_creator_container.tsx
+++ app/src/components/market/market_wizard_creator_container.tsx
@@ -67,7 +67,6 @@ const MarketWizardCreatorContainer: FC = () => {
         conditionalTokens.address,
         daiAddress,
         conditionId,
-        funding,
       )
       setMarketMakerAddress(marketMakerAddress)
 
