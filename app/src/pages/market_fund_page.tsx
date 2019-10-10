06872f1c84e50696228aadf1454283dc1159cd35
Mariano Aguero <mariano.aguero@altoros.com>
2019-10-10 18:40:54 -0300

    Add market fund page

---
+++ app/src/pages/market_fund_page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { RouteComponentProps } from 'react-router'
+
+import { MarketFundContainer } from '../components'
+import { ConnectedWeb3, useConnectWeb3 } from '../hooks/connectedWeb3'
+
+interface RouteParams {
+  address: string
+}
+
+const MarketFundPage = (props: RouteComponentProps<RouteParams>) => {
+  useConnectWeb3()
+
+  return (
+    <ConnectedWeb3>
+      <MarketFundContainer marketAddress={props.match.params.address} />
+    </ConnectedWeb3>
+  )
+}
+
+export { MarketFundPage }
