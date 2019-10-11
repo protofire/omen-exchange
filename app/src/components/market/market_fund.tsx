5adddc3acb1850faddb09588b4d1b11f1ed08109
Mariano Aguero <mariano.aguero@altoros.com>
2019-10-11 17:58:25 -0300

    Add useMarketMakerData and tables to display data

--- app/src/components/market/market_fund.tsx
+++ app/src/components/market/market_fund.tsx
@@ -1,21 +1,83 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+import { BigNumber } from 'ethers/utils'
 
 import { SectionTitle } from '../common/section_title'
 import { formatDate } from '../../util/tools'
+import { ViewCard } from '../common/view_card/'
+import { Table, TD, TR } from '../common/table'
+import { FormLabel } from '../common/form_label'
+import { ethers } from 'ethers'
+import { BalanceItem, OutcomeTableValue, Status, WinnerOutcome } from '../../util/types'
+import { OutcomeTable } from '../common/outcome_table'
+import { FullLoading } from '../common/full_loading'
 
 interface Props {
   marketMakerAddress: string
   question: string
   resolution: Maybe<Date>
+  totalPoolShares: BigNumber
+  userPoolShares: BigNumber
+  marketFunding: BigNumber
+  userPoolSharesPercentage: number
+  balance: BalanceItem[]
+  winnerOutcome: Maybe<WinnerOutcome>
+  status: Status
 }
 
+const TableStyled = styled(Table)`
+  margin-bottom: 30px;
+`
+
+const FormLabelStyled = styled(FormLabel)`
+  margin-bottom: 10px;
+`
+
 const MarketFund: FC<Props> = props => {
-  const { question, resolution, marketMakerAddress } = props
+  const {
+    question,
+    resolution,
+    totalPoolShares,
+    userPoolShares,
+    userPoolSharesPercentage,
+    balance,
+    status,
+  } = props
 
   return (
     <>
       <SectionTitle title={question} subTitle={resolution ? formatDate(resolution) : ''} />
-      <>Market fund address {marketMakerAddress}</>
+      <ViewCard>
+        <FormLabelStyled>Totals</FormLabelStyled>
+        <TableStyled>
+          <TR>
+            <TD>Total pool shares</TD>
+            <TD textAlign="right">
+              {totalPoolShares ? ethers.utils.formatUnits(totalPoolShares, 18) : '0'}{' '}
+              <strong>shares</strong>
+            </TD>
+          </TR>
+          <TR>
+            <TD>Your pool shares</TD>
+            <TD textAlign="right">
+              {userPoolShares ? ethers.utils.formatUnits(userPoolShares, 18) : '0'} (
+              {userPoolSharesPercentage ? userPoolSharesPercentage.toFixed(2) : '0'} %){' '}
+              <strong>shares</strong>
+            </TD>
+          </TR>
+        </TableStyled>
+        <OutcomeTable
+          balance={balance}
+          disabledColumns={[
+            OutcomeTableValue.Probabilities,
+            OutcomeTableValue.CurrentPrice,
+            OutcomeTableValue.Payout,
+            OutcomeTableValue.PriceAfterTrade,
+          ]}
+          displayRadioSelection={false}
+        />
+      </ViewCard>
+      {status === Status.Loading ? <FullLoading /> : null}
     </>
   )
 }
--- app/src/components/market/market_fund_container.tsx
+++ app/src/components/market/market_fund_container.tsx
@@ -3,6 +3,7 @@ import React, { FC, useState } from 'react'
 import { MarketFund } from './market_fund'
 import { useConnectedWeb3Context } from '../../hooks/connectedWeb3'
 import { useQuestion } from '../../hooks/useQuestion'
+import { useMarketMakerData } from '../../hooks/useMarketMakerData'
 
 interface Props {
   marketMakerAddress: string
@@ -11,13 +12,30 @@ interface Props {
 const MarketFundContainer: FC<Props> = props => {
   const context = useConnectedWeb3Context()
   const [marketMakerAddress] = useState<string>(props.marketMakerAddress)
+
   const { question, resolution } = useQuestion(marketMakerAddress, context)
+  const {
+    totalPoolShares,
+    userPoolShares,
+    marketFunding,
+    balance,
+    userPoolSharesPercentage,
+    winnerOutcome,
+    status,
+  } = useMarketMakerData(marketMakerAddress, context)
 
   return (
     <MarketFund
       marketMakerAddress={marketMakerAddress}
       question={question || ''}
       resolution={resolution}
+      totalPoolShares={totalPoolShares}
+      userPoolShares={userPoolShares}
+      marketFunding={marketFunding}
+      balance={balance}
+      userPoolSharesPercentage={userPoolSharesPercentage}
+      winnerOutcome={winnerOutcome}
+      status={status}
     />
   )
 }
