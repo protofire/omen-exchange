512956e5583b5aa5f5b3932d0324d33f7f83b5a5
Mariano Aguero <mariano.aguero@altoros.com>
2019-10-11 17:53:44 -0300

    Remove useMemo

--- app/src/hooks/useQuestion.tsx
+++ app/src/hooks/useQuestion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
 import { ConnectedWeb3Context } from './connectedWeb3'
 import { MarketMakerService } from '../services'
