d315137f9a6f50efb724cabd7a4f69c762cc4224
Mariano Aguero <mariano.aguero@altoros.com>
2019-10-29 23:41:44 -0300

    Add commit ref in footer

--- app/src/common/constants.ts
+++ app/src/common/constants.ts
@@ -7,3 +7,4 @@ export const THREEBOX_ADMIN_ADDRESS: string =
   process.env.REACT_APP_THREEBOX_ADMIN_ADDRESS || '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1'
 export const THREEBOX_SPACE_NAME: string =
   process.env.REACT_APP_THREEBOX_SPACE_NAME || 'conditional_exchange'
+export const GIT_COMMIT: string = process.env.COMMIT_REF || ''
