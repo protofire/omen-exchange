62ce6245ca100ce3ecf001355dc3643cd263789c
Mariano Aguero <mariano.aguero@altoros.com>
2019-11-04 15:10:05 -0300

    Add filter for markets that I own

--- app/src/components/market/market_home.tsx
+++ app/src/components/market/market_home.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+
 import { MarketAndQuestion, Status } from '../../util/types'
 import { FullLoading } from '../common/full_loading'
+import { useConnectedWeb3Context } from '../../hooks/connectedWeb3'
 
 interface Props {
   status: Status
@@ -8,11 +10,38 @@ interface Props {
 }
 
 export const MarketHome = (props: Props) => {
+  const context = useConnectedWeb3Context()
+  const [marketsFiltered, setMarketsFiltered] = useState<MarketAndQuestion[]>([])
+
   const { status, markets } = props
+
+  useEffect(() => {
+    setMarketsFiltered(markets)
+  }, [markets])
+
+  const filterMarkets = (event: ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked
+
+    if (isChecked) {
+      setMarketsFiltered(
+        markets.filter(
+          (market: MarketAndQuestion) =>
+            market.ownerAddress.toLowerCase() === context.account.toLowerCase(),
+        ),
+      )
+    } else {
+      setMarketsFiltered(markets)
+    }
+  }
+
   return (
     <>
       <div>
-        <pre>{JSON.stringify(markets, null, 2)}</pre>
+        <input onChange={filterMarkets} type="checkbox" />
+        <label>Markets that I own</label>
+      </div>
+      <div>
+        <pre>{JSON.stringify(marketsFiltered, null, 2)}</pre>
       </div>
       {status === Status.Loading ? <FullLoading message="Loading markets..." /> : null}
     </>
