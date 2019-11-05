92f64bc22a77c50c0ff30f48fdf2f0b0dc7eff3d
Mariano Aguero <mariano.aguero@altoros.com>
2019-11-05 11:03:28 -0300

    Add realitio libraries

--- app/src/global.d.ts
+++ app/src/global.d.ts
@@ -1,6 +1,7 @@
 declare module '@walletconnect/web3-subprovider'
 declare module '3box'
 declare module '3box-comments-react'
+declare module '@realitio/realitio-lib/formatters/question'
 
 declare type Maybe<T> = T | null
 
