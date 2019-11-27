64cac6ff29d74e422f0ebb4a553f6a95da6263cf
Mariano Aguero <mariano.aguero@altoros.com>
2019-11-27 11:10:55 -0300

    Add ButtonConnectWallet component

---
+++ app/src/components/common/button_connect_wallet/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useWeb3Context } from 'web3-react'
+import styled from 'styled-components'
+
+const ButtonConnectWalletStyled = styled.div`
+  width: 166px;
+  height: 28px;
+  border-radius: 2px;
+  background-color: #ff7848;
+  cursor: pointer;
+`
+
+const ButtonTitle = styled.div`
+  width: 144px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.38;
+  letter-spacing: normal;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 5.4px;
+  margin-left: 11px;
+`
+
+interface Props {
+  onClick: () => void
+  modalState: boolean
+}
+
+export const ButtonConnectWallet = (props: Props) => {
+  const context = useWeb3Context()
+
+  if (context.error) {
+    console.error('Error in web3 context', context.error)
+  }
+
+  const buttonMessage = props.modalState ? 'CONNECTING...' : 'CONNECT TO A WALLET'
+
+  return (
+    <>
+      {!context.active && (
+        <ButtonConnectWalletStyled
+          onClick={() => {
+            props.onClick()
+          }}
+        >
+          <ButtonTitle>{buttonMessage}</ButtonTitle>
+        </ButtonConnectWalletStyled>
+      )}
+    </>
+  )
+}
