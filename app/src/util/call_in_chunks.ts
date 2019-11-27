9966c4f54ea837b3fcbe7ea97a434b2f898dd6fe
Franco Victorio <victorio.franco@gmail.com>
2019-11-27 18:11:10 -0300

    Add tests for "call_in_chunks"

--- app/src/util/call_in_chunks.ts
+++ app/src/util/call_in_chunks.ts
@@ -1,14 +1,35 @@
 import { delay } from './tools'
 
+/**
+ * Calls function `f` repeatedly over the given `range`, dividing this range by `chunkSize`
+ *
+ * For example, if the range is [0, 11] and the chunk size is 3, then `f` will be called like this:
+ *
+ * f([9, 11])
+ * f([6, 8])
+ * f([3, 5])
+ * f([0, 2])
+ *
+ * `f` must return an array, and the final result is obtained from concatenating these partial results.
+ *
+ * A `options.callUntil` callback can be specified. This function receives the accumulated result so far and returns a boolean.
+ * If the response is `true`, the function finishes with this result.
+ *
+ * If `options.delay` is specified, then after each call to `f` there will be a delay of `options.delay` milliseconds.
+ *
+ * Returns the accumulated result, and the effective range that was used (equal to the input range if not `callUntil`
+ * was specified).
+ */
 export async function callInChunks<T>(
   f: (subrange: [number, number]) => Promise<T[]>,
-  [start, end]: [number, number],
+  range: [number, number],
   options: {
     chunkSize: number
     delay?: number
     callUntil?: (result: T[]) => boolean
   },
 ): Promise<[T[], [number, number]]> {
+  const [start, end] = range
   let result: T[] = []
 
   let subrangeStart = end - options.chunkSize + 1
@@ -20,7 +41,7 @@ export async function callInChunks<T>(
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const subresult = await f([subrangeStart, subrangeEnd])
-    result = result.concat(subresult)
+    result = subresult.concat(result)
 
     if (subrangeStart === start) {
       break
@@ -31,7 +52,7 @@ export async function callInChunks<T>(
     }
 
     subrangeEnd = subrangeStart - 1
-    subrangeStart = subrangeStart - options.chunkSize + 1
+    subrangeStart = subrangeStart - options.chunkSize
     if (subrangeStart < start) {
       subrangeStart = start
     }
@@ -41,5 +62,5 @@ export async function callInChunks<T>(
     }
   }
 
-  return [result, [subrangeStart, subrangeEnd]]
+  return [result, [subrangeStart, end]]
 }
