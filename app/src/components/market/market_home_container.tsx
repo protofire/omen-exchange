6997868db6179f47632c308339a059459154061f
Franco Victorio <victorio.franco@gmail.com>
2019-12-02 11:22:02 -0300

    Add infinite scroll behavior with react-waypoint

--- app/src/components/market/market_home_container.tsx
+++ app/src/components/market/market_home_container.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
+import { Waypoint } from 'react-waypoint'
 
 import { MarketHome } from './market_home'
 import { useConnectedWeb3Context } from '../../hooks/connectedWeb3'
 import { useMarkets } from '../../hooks/useMarkets'
+import { RemoteData } from '../../util/remote_data'
 import { MarketFilters } from '../../util/types'
 
-const PAGE_SIZE = 3
+const PAGE_SIZE = 10
 
 const MarketHomeContainer: React.FC = () => {
   const context = useConnectedWeb3Context()
@@ -22,15 +24,18 @@ const MarketHomeContainer: React.FC = () => {
   }
 
   return (
-    <MarketHome
-      markets={markets}
-      count={count}
-      moreMarkets={moreMarkets}
-      context={context}
-      currentFilter={filter}
-      onFilterChange={onFilterChange}
-      onShowMore={showMore}
-    />
+    <>
+      <MarketHome
+        markets={markets}
+        count={count}
+        moreMarkets={moreMarkets}
+        context={context}
+        currentFilter={filter}
+        onFilterChange={onFilterChange}
+        onShowMore={showMore}
+      />
+      {RemoteData.is.success(markets) && <Waypoint onEnter={showMore} />}
+    </>
   )
 }
 
