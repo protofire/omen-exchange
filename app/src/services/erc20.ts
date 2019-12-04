1c02c4781b998f19f82e8758f0a32c0872e02c1d
Mariano Aguero <mariano.aguero@altoros.com>
2019-12-04 11:11:10 -0300

    Add some new methods, and provider as a property

--- app/src/services/erc20.ts
+++ app/src/services/erc20.ts
@@ -2,6 +2,8 @@ import { ethers, Wallet } from 'ethers'
 import { BigNumber } from 'ethers/utils'
 
 import { getLogger } from '../util/logger'
+import { isAddress, isContract } from '../util/tools'
+import { Collateral } from '../util/types'
 
 const logger = getLogger('Services::Erc20')
 
@@ -9,25 +11,29 @@ const erc20Abi = [
   'function allowance(address owner, address spender) external view returns (uint256)',
   'function approve(address spender, uint256 amount) external returns (bool)',
   'function balanceOf(address marketMaker) external view returns (uint256)',
+  'function symbol() external view returns (string)',
+  'function name() external view returns (string)',
+  'function decimals() external view returns (uint8)',
 ]
 
 class ERC20Service {
   tokenAddress: string
+  provider: any
 
-  constructor(tokenAddress: string) {
+  constructor(provider: any, tokenAddress: string) {
     this.tokenAddress = tokenAddress
+    this.provider = provider
   }
 
   /**
    * @returns A boolean indicating if `spender` has enough allowance to transfer `neededAmount` tokens from `spender`.
    */
   hasEnoughAllowance = async (
-    provider: any,
     owner: string,
     spender: string,
     neededAmount: BigNumber,
   ): Promise<boolean> => {
-    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, provider)
+    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, this.provider)
 
     const allowance: BigNumber = await erc20Contract.allowance(owner, spender)
 
@@ -37,39 +43,87 @@ class ERC20Service {
   /**
    * Approve `spender` to transfer `amount` tokens on behalf of the connected user.
    */
-  approve = async (provider: any, spender: string, amount: BigNumber): Promise<any> => {
-    const signer: Wallet = provider.getSigner()
+  approve = async (spender: string, amount: BigNumber): Promise<any> => {
+    const signer: Wallet = this.provider.getSigner()
 
-    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, provider).connect(signer)
+    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, this.provider).connect(
+      signer,
+    )
 
     const transactionObject = await erc20Contract.approve(spender, amount, {
       value: '0x0',
     })
     logger.log(`Approve transaccion hash: ${transactionObject.hash}`)
-    await provider.waitForTransaction(transactionObject.hash)
+    await this.provider.waitForTransaction(transactionObject.hash)
   }
 
   /**
    * Approve `spender` to transfer an "unlimited" amount of tokens on behalf of the connected user.
    */
-  approveUnlimited = async (provider: any, spender: string): Promise<any> => {
-    const signer: Wallet = provider.getSigner()
+  approveUnlimited = async (spender: string): Promise<any> => {
+    const signer: Wallet = this.provider.getSigner()
 
-    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, provider).connect(signer)
+    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, this.provider).connect(
+      signer,
+    )
 
     const transactionObject = await erc20Contract.approve(spender, ethers.constants.MaxUint256, {
       value: '0x0',
     })
     logger.log(`Approve unlimited transaccion hash: ${transactionObject.hash}`)
-    await provider.waitForTransaction(transactionObject.hash)
+    await this.provider.waitForTransaction(transactionObject.hash)
   }
 
-  getCollateral = async (marketMakerAddress: string, provider: any): Promise<any> => {
-    const signer: Wallet = provider.getSigner()
+  getCollateral = async (marketMakerAddress: string): Promise<any> => {
+    const signer: Wallet = this.provider.getSigner()
 
-    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, provider).connect(signer)
+    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, this.provider).connect(
+      signer,
+    )
 
     return erc20Contract.balanceOf(marketMakerAddress)
+  }
+
+  isValidErc20 = async (): Promise<boolean> => {
+    try {
+      if (!isAddress(this.tokenAddress)) {
+        throw new Error('Is not a valid erc20 address')
+      }
+
+      if (!isContract(this.provider, this.tokenAddress)) {
+        throw new Error('Is not a valid contract')
+      }
+
+      const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, this.provider)
+
+      const [decimals, name, symbol] = await Promise.all([
+        erc20Contract.decimals(),
+        erc20Contract.name(),
+        erc20Contract.symbol(),
+      ])
+
+      return !!(decimals && name && symbol)
+    } catch (err) {
+      logger.error(err.message)
+      return false
+    }
+  }
+
+  getProfileSummary = async (): Promise<Collateral> => {
+    const erc20Contract = new ethers.Contract(this.tokenAddress, erc20Abi, this.provider)
+
+    const [decimals, name, symbol] = await Promise.all([
+      erc20Contract.decimals(),
+      erc20Contract.name(),
+      erc20Contract.symbol(),
+    ])
+
+    return {
+      address: this.tokenAddress,
+      decimals,
+      name,
+      symbol,
+    }
   }
 }
 
