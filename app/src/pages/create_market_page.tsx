857557e132e572cc9df44c61ef1ebc9b3c5861cb
Mariano Aguero <mariano.aguero@altoros.com>
2019-12-26 16:09:29 -0300

    Remove props and dist

--- app/src/pages/create_market_page.tsx
+++ app/src/pages/create_market_page.tsx
@@ -3,34 +3,34 @@ import React from 'react'
 import { MarketWizardCreatorContainer } from '../components'
 import connectors from '../util/connectors'
 import { ConnectedWeb3, useConnectWeb3 } from '../hooks/connectedWeb3'
-import { useWeb3Context } from 'web3-react/dist'
+import { useWeb3Context } from 'web3-react'
 
-const CreateMarketConnectedPage = (props: any) => {
+const CreateMarketConnectedPage = () => {
   useConnectWeb3()
 
   return (
     <ConnectedWeb3>
-      <MarketWizardCreatorContainer {...props} />
+      <MarketWizardCreatorContainer />
     </ConnectedWeb3>
   )
 }
 
-const CreateMarketDisconnectedPage = (props: any) => {
+const CreateMarketDisconnectedPage = () => {
   return (
     <ConnectedWeb3 infura={true}>
-      <MarketWizardCreatorContainer {...props} />
+      <MarketWizardCreatorContainer />
     </ConnectedWeb3>
   )
 }
 
-const CreateMarketPage = (props: any) => {
+const CreateMarketPage = () => {
   const { active } = useWeb3Context()
   const connector = localStorage.getItem('CONNECTOR')
 
   return active || (connector && connector in connectors) ? (
-    <CreateMarketConnectedPage {...props} />
+    <CreateMarketConnectedPage />
   ) : (
-    <CreateMarketDisconnectedPage {...props} />
+    <CreateMarketDisconnectedPage />
   )
 }
 
