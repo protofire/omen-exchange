90eb5909c30d5f4751553b160c6b982602bb822e
Mariano Aguero <mariano.aguero@altoros.com>
2019-12-27 14:50:25 -0300

    Remove unnecessary close modal

--- app/src/components/common/modal_connect_wallet/index.tsx
+++ app/src/components/common/modal_connect_wallet/index.tsx
@@ -140,7 +140,6 @@ export const ModalConnectWallet = (props: Props) => {
       WalletConnectQRCodeModal.open(uri, () => {
         // Callback passed to the onClose click of the QRCode modal
         onClickCloseButton()
-        WalletConnectQRCodeModal.close()
         localStorage.removeItem('CONNECTOR')
         context.unsetConnector()
       })
