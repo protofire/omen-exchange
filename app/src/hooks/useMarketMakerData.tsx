6407b4c5d556a6c68f61f3c912ec855f501ca085
Mariano Aguero <mariano.aguero@altoros.com>
2020-01-06 16:21:54 -0300

    Remove empty string

--- app/src/hooks/useMarketMakerData.tsx
+++ app/src/hooks/useMarketMakerData.tsx
@@ -61,7 +61,6 @@ export const useMarketMakerData = (
 
   const fetchFunc = useCallback(async () => {
     setStatus(status => (status === Status.Ready ? Status.Loading : Status.Refreshing))
-    const user = account || ''
 
     const marketMaker = buildMarketMaker(marketMakerAddress)
 
@@ -90,15 +89,15 @@ export const useMarketMakerData = (
       fee,
       isQuestionFinalized,
     ] = await Promise.all([
-      user
-        ? marketMaker.getBalanceInformation(user, outcomes.length)
+      account
+        ? marketMaker.getBalanceInformation(account, outcomes.length)
         : outcomes.map(() => new BigNumber(0)),
       marketMaker.getBalanceInformation(marketMakerAddress, outcomes.length),
       marketMaker.getTotalSupply(),
-      user ? marketMaker.balanceOf(user) : new BigNumber(0),
+      account ? marketMaker.balanceOf(account) : new BigNumber(0),
       marketMaker.getCollateralToken(),
       marketMaker.poolSharesTotalSupply(),
-      user ? marketMaker.poolSharesBalanceOf(user) : new BigNumber(0),
+      account ? marketMaker.poolSharesBalanceOf(account) : new BigNumber(0),
       marketMaker.getFee(),
       realitio.isFinalized(questionId),
     ])
