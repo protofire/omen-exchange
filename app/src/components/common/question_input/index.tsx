291a9c02274fafa7ead3612f5351bb7f6befe996
Mariano Aguero <mariano.aguero@altoros.com>
2020-01-09 11:30:27 -0300

    Add question input component

---
+++ app/src/components/common/question_input/index.tsx
@@ -0,0 +1,63 @@
+import React, { ChangeEvent, useState } from 'react'
+import styled from 'styled-components'
+
+import { ModalQuestion } from './modal_question'
+import { Question } from '../../../util/types'
+import { ConnectedWeb3Context } from '../../../hooks/connectedWeb3'
+import { FormRowLink } from '../form_row_link'
+import { Textfield } from '../textfield'
+
+interface Props {
+  defaultValue: string
+  name: string
+  onChange: (event: ChangeEvent<HTMLInputElement>) => any
+  onChangeFromRealitio: (question: Question) => any
+  onClearQuestionFromRealitio: () => void
+  placeholder: string
+  context: ConnectedWeb3Context
+  disabled: boolean
+}
+
+const Link = styled(FormRowLink)`
+  display: block;
+  margin-top: 5px;
+  margin-left: auto;
+`
+
+export const QuestionInput = (props: Props) => {
+  const {
+    defaultValue,
+    name = 'question',
+    onChange,
+    onChangeFromRealitio,
+    onClearQuestionFromRealitio,
+    placeholder = 'Type in a question...',
+    context,
+    disabled,
+  } = props
+
+  const [isModalQuestionOpen, setModalQuestionState] = useState(false)
+
+  return (
+    <>
+      <Textfield
+        defaultValue={defaultValue}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        type="text"
+      />
+      {!disabled && <Link onClick={() => setModalQuestionState(true)}>Add question</Link>}
+      {disabled && <Link onClick={onClearQuestionFromRealitio}>Clear question</Link>}
+      <ModalQuestion
+        context={context}
+        isOpen={isModalQuestionOpen}
+        onClose={() => setModalQuestionState(false)}
+        onSave={(question: Question) => {
+          onChangeFromRealitio(question)
+        }}
+      />
+    </>
+  )
+}
