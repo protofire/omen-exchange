56bf2c35187e02a296c8ee8f1bc956b41adb737b
Mariano Aguero <mariano.aguero@altoros.com>
2020-01-09 18:43:52 -0300

    Remove unused components

--- app/src/pages/create_market_page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { MarketWizardCreatorContainer } from '../components'
-
-const CreateMarketPage = () => {
-  return <MarketWizardCreatorContainer />
-}
-
-export { CreateMarketPage }
--- app/src/pages/market_home_page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { MarketHomeContainer } from '../components'
-
-const MarketHomePage = (props: any) => {
-  return <MarketHomeContainer {...props} />
-}
-
-export { MarketHomePage }
