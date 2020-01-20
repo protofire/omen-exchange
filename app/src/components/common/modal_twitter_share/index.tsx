8a6a3eb19c34d129a65b8d354035a65d0099b7ff
Agustín Longoni <agustin.longoni@altoros.com>
2020-01-20 09:52:29 -0300

    Add styles to modal - share on twitter

--- app/src/components/common/modal_twitter_share/index.tsx
+++ app/src/components/common/modal_twitter_share/index.tsx
@@ -1,6 +1,8 @@
 import React, { HTMLAttributes } from 'react'
 
+import styled from 'styled-components'
 import ModalWrapper from '../modal_wrapper'
+import { Well } from '../well'
 import { TwitterIcon, TwitterShareButton } from 'react-share'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -11,15 +13,50 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   onClose: () => void
 }
 
+
+const OutcomeInfo = styled(Well)`
+  margin-bottom: 30px;
+`
+
+const ButtonContainer = styled.div`
+    float: right;
+`
+
+const TwitterGlobalButton = styled.button`
+  color: white;
+  background-color: #00aced;
+  border: 0;
+  border-radius: 3px;
+  width: 92px;
+  float: right;
+
+  div {
+    float: left;
+  }
+`
+
+const Text = styled.p`
+    margin: 8px 0px;
+    float: right;
+`
+
 export const ModalTwitterShare = (props: Props) => {
   const { title, shareUrl, description, isOpen, onClose } = props
 
   return (
     <ModalWrapper isOpen={isOpen} onRequestClose={onClose} title={title}>
-      {description}
-      <TwitterShareButton url={shareUrl} title={description}>
-        <TwitterIcon size={32} />
-      </TwitterShareButton>
+      <OutcomeInfo>
+      {description}   
+      </OutcomeInfo>
+
+      <ButtonContainer>
+        <TwitterGlobalButton>
+          <TwitterShareButton url={shareUrl} title={description}>
+            <TwitterIcon size={32} /> 
+            <Text>Tweet</Text> 
+          </TwitterShareButton>
+        </TwitterGlobalButton>
+      </ButtonContainer>
     </ModalWrapper>
   )
 }
