de7ef7e516aa3a2f49b6a4873c0468c6f2c8b08d
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-03-02 16:59:36 -0300

    (add) header buttons

--- app/src/components/common/header/index.tsx
+++ app/src/components/common/header/index.tsx
@@ -5,13 +5,15 @@ import styled from 'styled-components'
 import { useWeb3Context } from 'web3-react/dist'
 
 import { ConnectedWeb3 } from '../../../hooks/connectedWeb3'
+import { ButtonType } from '../../../theme/component_styles/button_styling_types'
+import { Button } from '../button'
 import { ButtonConnectWallet } from '../button_connect_wallet'
-import { ButtonDisconnectWallet } from '../button_disconnect_wallet'
 import { Logo } from '../logo'
 import { ModalConnectWallet } from '../modal_connect_wallet'
 import { Network } from '../network'
 
 const HeaderWrapper = styled.div`
+  align-items: flex-end;
   background: ${props => props.theme.header.backgroundColor};
   display: flex;
   flex-grow: 0;
@@ -40,26 +42,16 @@ const HeaderInner = styled.div`
 `
 
 const NetworkStyled = styled(Network)`
-  margin: 0 0 0 auto;
-
-  @media (max-width: ${props => props.theme.themeBreakPoints.md}) {
-    background-color: #fefefe;
-    border-bottom: 1px solid ${props => props.theme.borders.borderColor};
-    height: 23px;
-    justify-content: flex-end;
-    left: 0;
-    padding: 0 10px;
-    position: absolute;
-    top: calc(100% + 1px);
-    width: 100%;
-  }
-`
-
-const ButtonDisconnectWalletStyled = styled(ButtonDisconnectWallet)`
-  margin: 0 0 0 25px;
+  margin: 0 0 0 12px;
 `
 
 const ButtonConnectWalletStyled = styled(ButtonConnectWallet)`
+  margin: 0 0 0 12px;
+`
+
+const ContentsRight = styled.div`
+  align-items: center;
+  display: flex;
   margin: 0 0 0 auto;
 `
 
@@ -68,7 +60,7 @@ const LogoWrapper = styled(NavLink)``
 const HeaderContainer: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
   const context = useWeb3Context()
 
-  const { ...restProps } = props
+  const { history, ...restProps } = props
   const [isModalOpen, setModalState] = useState(false)
 
   return (
@@ -77,22 +69,22 @@ const HeaderContainer: React.FC<RouteComponentProps> = (props: RouteComponentPro
         <LogoWrapper to="/">
           <Logo />
         </LogoWrapper>
-        <ConnectedWeb3>
-          <NetworkStyled />
-          <ButtonDisconnectWalletStyled
-            callback={() => {
-              setModalState(false)
-            }}
-          />
-        </ConnectedWeb3>
-        {!context.account && (
-          <ButtonConnectWalletStyled
-            modalState={isModalOpen}
-            onClick={() => {
-              setModalState(true)
-            }}
-          />
-        )}
+        <ContentsRight>
+          <Button buttonType={ButtonType.secondaryLine} onClick={() => history.push('/create')}>
+            Create Market
+          </Button>
+          {!context.account && (
+            <ButtonConnectWalletStyled
+              modalState={isModalOpen}
+              onClick={() => {
+                setModalState(true)
+              }}
+            />
+          )}
+          <ConnectedWeb3>
+            <NetworkStyled />
+          </ConnectedWeb3>
+        </ContentsRight>
         <ModalConnectWallet isOpen={isModalOpen} onClose={() => setModalState(false)} />
       </HeaderInner>
     </HeaderWrapper>
--- app/src/components/common/network/index.tsx
+++ app/src/components/common/network/index.tsx
@@ -1,50 +1,15 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 import { useConnectedWeb3Context } from '../../../hooks/connectedWeb3'
-import { getContractAddressName } from '../../../util/networks'
+import { ButtonType } from '../../../theme/component_styles/button_styling_types'
 import { truncateStringInTheMiddle } from '../../../util/tools'
+import { Button } from '../button'
 
-import Icon from './img/icon.svg'
+import { ConnectionIcon } from './img/ConnectionIcon'
 
-const NetworkWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-`
-
-const NetworkContainer = styled.div`
-  align-items: center;
-  display: flex;
-`
-
-const IconStyled = styled.img`
-  height: 13px;
-  margin: 0 8px 0 0;
-  width: 13px;
-`
-
-const Separator = css`
-  &::before {
-    background-color: ${props => props.theme.header.color};
-    content: '';
-    height: 22px;
-    margin: 0 15px;
-    width: 1px;
-  }
-`
-
-const NetworkName = styled.span`
-  color: ${props => props.theme.header.color};
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 1.2;
-`
-
-const ConnectionStatusWrapper = styled.div`
-  ${Separator}
-  align-items: center;
-  display: flex;
+const Wrapper = styled(Button)`
+  padding-left: 12px;
 `
 
 const ConnectionStatusText = styled.span`
@@ -55,34 +20,25 @@ const ConnectionStatusText = styled.span`
 `
 
 const ConnectionStatusDot = styled.div`
-  background-color: #55bc65;
-  border-radius: 50%;
-  height: 12px;
-  margin-right: 8px;
-  width: 12px;
+  height: 20px;
+  margin-right: 5px;
+  width: 20px;
 `
 
 export const Network: React.FC = props => {
   const context = useConnectedWeb3Context()
-  const { account, networkId } = context
-  const networkName = getContractAddressName(networkId)
+  const { account } = context
   const { ...restProps } = props
 
   if (!account) {
     return null
   }
   return (
-    <NetworkWrapper {...restProps}>
-      <NetworkContainer>
-        <IconStyled alt="" src={Icon} />
-        {networkName ? <NetworkName>{networkName}</NetworkName> : null}
-      </NetworkContainer>
-      <ConnectionStatusWrapper>
-        <ConnectionStatusDot />
-        <ConnectionStatusText>
-          {truncateStringInTheMiddle(account, 6, 4) || 'No account connected'}
-        </ConnectionStatusText>
-      </ConnectionStatusWrapper>
-    </NetworkWrapper>
+    <Wrapper buttonType={ButtonType.secondaryLine} {...restProps}>
+      <ConnectionStatusDot>
+        <ConnectionIcon />
+      </ConnectionStatusDot>
+      <ConnectionStatusText>{truncateStringInTheMiddle(account, 6, 4) || 'No account connected'}</ConnectionStatusText>
+    </Wrapper>
   )
 }
