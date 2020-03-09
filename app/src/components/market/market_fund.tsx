a9d5950e22afbd82ab4448ed11bd13d16f72dc0e
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-03-09 14:21:19 -0300

    (fix) imports

--- app/src/components/market/market_fund.tsx
+++ app/src/components/market/market_fund.tsx
@@ -11,7 +11,7 @@ import { ERC20Service } from '../../services'
 import { CPKService } from '../../services/cpk'
 import { ButtonType } from '../../theme/component_styles/button_styling_types'
 import { getLogger } from '../../util/logger'
-import { divBN, formatBigNumber, formatDate } from '../../util/tools'
+import { divBN, formatBigNumber } from '../../util/tools'
 import { BalanceItem, OutcomeTableValue, Status, Token } from '../../util/types'
 import { BalanceShares } from '../common/balance_shares'
 import { BalanceToken } from '../common/balance_token'
@@ -83,7 +83,6 @@ const MarketFundWrapper: React.FC<Props> = (props: Props) => {
     marketMakerFunding,
     marketMakerUserFunding,
     question,
-    resolution,
     totalPoolShares,
     userPoolShares,
   } = props
@@ -182,7 +181,7 @@ const MarketFundWrapper: React.FC<Props> = (props: Props) => {
 
   return (
     <>
-      <SectionTitle subTitle={resolution ? formatDate(resolution) : ''} title={question} />
+      <SectionTitle title={question} />
       <ViewCard>
         <SubsectionTitleStyled>Fund this market</SubsectionTitleStyled>
         <OutcomeTable
