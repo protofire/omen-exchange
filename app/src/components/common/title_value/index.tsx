24270070e3ca182ea22675478d16d88eeaf58adb
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-03-10 19:38:30 -0300

    (fix) title / value component responsiveness

--- app/src/components/common/title_value/index.tsx
+++ app/src/components/common/title_value/index.tsx
@@ -3,14 +3,9 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: space-between;
   margin: 0 0 10px;
-
-  @media (min-width: ${props => props.theme.themeBreakPoints.md}) {
-    flex-direction: row;
-    margin-bottom: 0;
-  }
 `
 
 const Title = styled.h2`
@@ -19,12 +14,8 @@ const Title = styled.h2`
   font-size: 14px;
   font-weight: 400;
   line-height: 1.2;
-  margin: 0 5px 5px 0;
+  margin: 0 5px 0 0;
   white-space: nowrap;
-
-  @media (min-width: ${props => props.theme.themeBreakPoints.md}) {
-    margin-bottom: 0;
-  }
 `
 
 const Value = styled.p`
