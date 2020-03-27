e3e41ba4b48584f3f370fe199256c4bac2d9dea8
Franco Victorio <victorio.franco@gmail.com>
2020-03-27 15:56:57 -0300

    Allow zero fee

--- app/src/components/market/steps/items/funding_and_fee_step.tsx
+++ app/src/components/market/steps/items/funding_and_fee_step.tsx
@@ -110,7 +110,7 @@ const FundingAndFeeStep = (props: Props) => {
   const collateralBalance = useCollateralBalance(collateral, context)
 
   const isFundingGreaterThanBalance = account ? funding.gt(collateralBalance) : false
-  const error = !spread || funding.isZero() || isFundingGreaterThanBalance
+  const error = funding.isZero() || isFundingGreaterThanBalance
 
   const fundingMessageError = isFundingGreaterThanBalance ? `You don't have enough collateral in your balance.` : ''
   const resolutionDate = resolution && formatDate(resolution)
