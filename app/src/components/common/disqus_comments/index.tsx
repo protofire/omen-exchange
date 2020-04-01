f79ed6824b15cf5f9c48979952d17fc286f2cc27
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-04-01 15:13:39 -0300

    (fix) diqus container

--- app/src/components/common/disqus_comments/index.tsx
+++ app/src/components/common/disqus_comments/index.tsx
@@ -1,7 +1,14 @@
 import Disqus from 'disqus-react'
 import React from 'react'
+import styled from 'styled-components'
 
 import { DISQUS_SHORTNAME, DISQUS_URL } from '../../../common/constants'
+
+const DisqusCustom = styled.div`
+  margin: 30px auto;
+  max-width: 100%;
+  width: ${props => props.theme.mainContainer.maxWidth};
+`
 
 export const DisqusComments: React.FC<{ marketMakerAddress: string }> = ({ marketMakerAddress }) => {
   const config = React.useMemo(() => {
@@ -12,5 +19,9 @@ export const DisqusComments: React.FC<{ marketMakerAddress: string }> = ({ marke
     }
   }, [marketMakerAddress])
 
-  return <Disqus.DiscussionEmbed config={config} shortname={DISQUS_SHORTNAME} />
+  return (
+    <DisqusCustom>
+      <Disqus.DiscussionEmbed config={config} shortname={DISQUS_SHORTNAME} />
+    </DisqusCustom>
+  )
 }
