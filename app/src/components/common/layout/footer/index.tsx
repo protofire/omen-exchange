20f581ac0329a4c0dd8b309b17501bb7ccdca6c5
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-04-16 21:39:33 -0300

    (change) disclaimer looks and behavior

--- app/src/components/common/layout/footer/index.tsx
+++ app/src/components/common/layout/footer/index.tsx
@@ -1,16 +1,14 @@
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 import {
-  DISCLAIMER_TEXT,
   LINK_COOKIE_POLICY,
   LINK_FAQ,
   LINK_PRIVACY_POLICY,
   LINK_TERMS_AND_CONDITIONS,
   SHOW_FOOTER,
 } from '../../../../common/constants'
-import { Button } from '../../../button'
-import { ButtonType } from '../../../button/button_styling_types'
+import { Disclaimer } from '../../disclaimer'
 
 const Wrapper = styled.div`
   align-items: flex-end;
@@ -18,7 +16,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-shrink: 0;
   font-size: 14px;
-  height: 40px;
+  height: 50px;
   justify-content: center;
   line-height: 1.2;
   width: 100%;
@@ -42,55 +40,7 @@ const Break = styled.span`
   }
 `
 
-const Disclaimer = styled.div`
-  align-items: center;
-  background: rgba(128, 128, 128, 0.9);
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  min-height: 40px;
-  justify-content: center;
-  left: 0;
-  position: fixed;
-  right: 0;
-  z-index: 5;
-`
-
-const DisclaimerText = styled.div`
-  color: #fff;
-  font-size: 13px;
-  font-weight: 500;
-  max-width: 900px;
-  padding: 10px;
-  text-align: center;
-  text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.3);
-`
-
-const ButtonStyled = styled(Button)`
-  font-size: 12px;
-  height: 20px;
-  margin-bottom: 10px;
-`
-
-enum DisclaimerStates {
-  hidden = 'hidden',
-  visible = 'visible',
-}
-
 export const Footer = () => {
-  const storage = window.localStorage
-  const [acceptedDisclaimer, setAcceptedDisclaimer] = useState<string | null>(DisclaimerStates.visible)
-
-  const toggleDisclaimer = useCallback(() => {
-    const toggledDisclaimerState =
-      acceptedDisclaimer === null || acceptedDisclaimer === DisclaimerStates.visible
-        ? DisclaimerStates.hidden
-        : DisclaimerStates.visible
-
-    setAcceptedDisclaimer(toggledDisclaimerState)
-    storage.setItem('acceptedDisclaimer', toggledDisclaimerState)
-  }, [acceptedDisclaimer, storage])
-
   return SHOW_FOOTER ? (
     <>
       <Wrapper>
@@ -126,22 +76,8 @@ export const Footer = () => {
             <Break>-</Break>
           </>
         )}
-        {DISCLAIMER_TEXT && (
-          <Link as="span" onClick={toggleDisclaimer}>
-            Disclaimer
-          </Link>
-        )}
       </Wrapper>
-      {DISCLAIMER_TEXT && acceptedDisclaimer !== DisclaimerStates.hidden && (
-        <Disclaimer>
-          <DisclaimerText>
-            <strong>Disclaimer:</strong> {DISCLAIMER_TEXT}
-          </DisclaimerText>
-          <ButtonStyled buttonType={ButtonType.primaryLine} onClick={toggleDisclaimer}>
-            OK
-          </ButtonStyled>
-        </Disclaimer>
-      )}
+      <Disclaimer />
     </>
   ) : null
 }
