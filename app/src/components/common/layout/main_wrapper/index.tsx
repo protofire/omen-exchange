3211a13707e2a52a9712c97039aa3399083b45e3
Maximiliano Molina <hpmaxi@gmail.com>
2020-04-22 11:24:04 -0300

    useLocation to emit ga events

--- app/src/components/common/layout/main_wrapper/index.tsx
+++ app/src/components/common/layout/main_wrapper/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import GoogleAnalytics from 'react-ga'
+import { useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
 const MainWrapperStyled = styled.div`
@@ -7,9 +9,16 @@ const MainWrapperStyled = styled.div`
   height: 100vh;
   width: 100vw;
 `
+const usePageViews = () => {
+  const location = useLocation()
 
+  React.useEffect(() => {
+    GoogleAnalytics.send(['pageview', location.pathname])
+  }, [location])
+}
 export const MainWrapper: React.FC = props => {
   const { children, ...restProps } = props
+  usePageViews()
 
   return <MainWrapperStyled {...restProps}>{children}</MainWrapperStyled>
 }
