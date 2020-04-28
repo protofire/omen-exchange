b83a07e137572554fcd07736fe62aa9c017fdbd6
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-04-28 12:35:32 -0300

    (fix) disconnect button states

--- app/src/components/button/button_disconnect_wallet/index.tsx
+++ app/src/components/button/button_disconnect_wallet/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import { useWeb3Context } from 'web3-react'
 
+import { Spinner } from '../../common/spinner'
 import { Button } from '../button'
 import { ButtonProps, ButtonType } from '../button_styling_types'
+
+const SpinnerStyled = styled(Spinner)`
+  margin-right: 10px;
+`
 
 interface Props extends ButtonProps {
   callback: () => void
@@ -12,22 +18,20 @@ export const ButtonDisconnectWallet = (props: Props) => {
   const { callback, ...restProps } = props
   const context = useWeb3Context()
   const { account, active, connectorName, error } = context
-
-  if (!account) {
-    return null
-  }
+  const [isWorking, setIsWorking] = useState(false)
 
   const logout = () => {
     if (active || (error && connectorName)) {
+      setIsWorking(true)
       callback()
       localStorage.removeItem('CONNECTOR')
       context.setConnector('Infura')
     }
   }
 
-  return (
-    <Button buttonType={ButtonType.secondaryLine} onClick={logout} {...restProps}>
-      Disconnect
+  return account ? (
+    <Button buttonType={ButtonType.secondaryLine} disabled={isWorking} onClick={logout} {...restProps}>
+      {isWorking && <SpinnerStyled height={'15px'} width={'15px'} />} Disconnect
     </Button>
-  )
+  ) : null
 }
