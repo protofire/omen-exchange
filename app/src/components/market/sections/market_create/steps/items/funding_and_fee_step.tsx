9abcf888923b962f320bf987daa85df306a7e61f
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-05-20 17:14:02 -0300

    (fix) date format

--- app/src/components/market/sections/market_create/steps/items/funding_and_fee_step.tsx
+++ app/src/components/market/sections/market_create/steps/items/funding_and_fee_step.tsx
@@ -203,8 +203,8 @@ const FundingAndFeeStep = (props: Props) => {
           </OutcomesTable>
         </OutcomesTableWrapper>
         <Grid>
-          <TitleValueVertical title={'Category'} value={category} />
           <TitleValueVertical title={'Earliest Resolution Date'} value={resolutionDate} />
+          <TitleValueVertical title={'Category'} value={category} />
           <TitleValueVertical title={'Arbitrator'} value={<DisplayArbitrator arbitrator={arbitrator} />} />
         </Grid>
       </CreateCardTop>
