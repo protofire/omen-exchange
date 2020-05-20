79837de98123aa9871fa4887c746bbc85ee96081
Maximiliano Molina <hpmaxi@gmail.com>
2020-05-20 16:49:04 -0300

    Use endpoint by networkid

--- app/src/hooks/useMultipleQueries.tsx
+++ app/src/hooks/useMultipleQueries.tsx
@@ -1,18 +1,24 @@
 import axios from 'axios'
 import { useCallback, useEffect, useState } from 'react'
 
+import { getGraphUris } from '../util/networks'
+
+import { useConnectedWeb3Context } from './connectedWeb3'
+
 type GraphVariables = { [key: string]: string }
-const fetchQuery = (query: string, variables: GraphVariables) => {
-  return axios.post('https://api.thegraph.com/subgraphs/name/gnosis/omen', { query, variables })
+const fetchQuery = (query: string, variables: GraphVariables, endpoint: string) => {
+  return axios.post(endpoint, { query, variables })
 }
 
 export const useMultipleQueries = <T,>(queries: string[], variables: GraphVariables): T[] => {
+  const { networkId } = useConnectedWeb3Context()
   const [queriesResult, setQueriesResult] = useState<T[]>([])
 
+  const { httpUri } = getGraphUris(networkId)
   const fetchQueries = useCallback(async () => {
-    const response = await Promise.all(queries.map(q => fetchQuery(q, variables)))
+    const response = await Promise.all(queries.map(q => fetchQuery(q, variables, httpUri)))
     setQueriesResult(response.filter(d => d && d.data).map(d => d.data))
-  }, [queries, variables])
+  }, [httpUri, queries, variables])
   useEffect(() => {
     fetchQueries()
   }, [fetchQueries])
