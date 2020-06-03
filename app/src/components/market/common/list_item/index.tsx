8a12cf297de70290beb7e98323c154dbb7ac740e
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-06-03 12:42:32 -0300

    (fix) wrap long words

--- app/src/components/market/common/list_item/index.tsx
+++ app/src/components/market/common/list_item/index.tsx
@@ -33,6 +33,9 @@ const Title = styled.h2`
   font-weight: 500;
   line-height: 1.2;
   margin: 0 0 5px 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `
 
 const Info = styled.div`
@@ -43,7 +46,9 @@ const Info = styled.div`
   font-size: 13px;
   font-weight: 700;
   line-height: 1.2;
-  white-space: nowrap;
+  overflow-wrap: break-word;
+  white-space: normal;
+  word-break: break-all;
 `
 
 const OutcomeTitle = styled.span`
