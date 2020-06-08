4495d240a647e7f99ef7ed2adde22fe2a7365a39
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-06-08 16:07:05 -0300

    (fix) resize comment's avatars

--- app/src/theme/overrides/threebox_styles_override.tsx
+++ app/src/theme/overrides/threebox_styles_override.tsx
@@ -2,6 +2,8 @@ import { css } from 'styled-components'
 
 import { ButtonCSS } from '../../components/button/button_styling_types'
 
+const COMMENT_AVATAR_DIMENSIONS = '32px'
+
 export const ThreeboxStylesOverride = css`
   .threeBoxCustom > .threebox-comments-react {
     max-width: 100%;
@@ -22,17 +24,44 @@ export const ThreeboxStylesOverride = css`
         width: 100%;
       }
     }
-  }
 
-  footer {
-    max-width: 100%;
-    .footer_text {
-      display: block;
-      text-align: center;
-    }
+        /* Comments list */
+        .dialogue {
+      .dialogue_grid {
+        display: block;
+        margin-bottom: 0;
+        row-gap: 30px;
+      }
 
-    * {
+      /* Comment */
+      .comment {
+        border-bottom: 1px solid ${props => props.theme.borders.borderColor};
+        padding: 15px 8px;
+
+        &:first-child {
+          border-top: 1px solid ${props => props.theme.borders.borderColor};
+        }
+
+        img {
+          height: ${COMMENT_AVATAR_DIMENSIONS};
+          max-height: ${COMMENT_AVATAR_DIMENSIONS};
+          max-width: ${COMMENT_AVATAR_DIMENSIONS};
+          min-height: ${COMMENT_AVATAR_DIMENSIONS};
+          min-width: ${COMMENT_AVATAR_DIMENSIONS};
+          width: ${COMMENT_AVATAR_DIMENSIONS};
+        }
+
+
+    footer {
       max-width: 100%;
+      .footer_text {
+        display: block;
+        text-align: center;
+      }
+
+      * {
+        max-width: 100%;
+      }
     }
   }
 `
