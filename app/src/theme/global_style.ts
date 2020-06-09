0dbe599e0aa837b3a15b020a3bab354104710c61
Nico Elzer <contact@nicoelzer.de>
2020-06-09 07:47:49 +0200

    Fixed build script

--- app/src/theme/global_style.ts
+++ app/src/theme/global_style.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 import { DatepickerStylesOverride } from './overrides/datepicker_styles_override'
 import { ReactTooltipStylesOverride } from './overrides/react_tooltip_styles_override'
+import { RechartsStylesOverride } from './overrides/recharts_styles_override'
 import { ThreeboxStylesOverride } from './overrides/threebox_styles_override'
 import { WalletConnectStylesOverride } from './overrides/wallet_connect_styles_override'
 
@@ -28,8 +29,10 @@ export const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
     height: 100vh;
   }
 
+  
   ${WalletConnectStylesOverride}
   ${DatepickerStylesOverride}
   ${ReactTooltipStylesOverride}
   ${ThreeboxStylesOverride}
+  ${RechartsStylesOverride}
 `
---
+++ app/src/theme/overrides/recharts_styles_override.tsx
@@ -0,0 +1,16 @@
+import { css } from 'styled-components'
+
+export const RechartsStylesOverride = css`
+  .recharts-tooltip-cursor {
+    z-index: 100;
+  }
+
+  .recharts-area-dots {
+    z-index: 100;
+  }
+
+  .recharts-dot {
+    z-index: 100;
+    position: absolute;
+  }
+`
