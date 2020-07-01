97f12cf01512cf9d94b480076983be723491a7e2
Maximiliano Molina <hpmaxi@gmail.com>
2020-07-01 12:37:21 -0300

    Fix linter errors

--- app/src/components/market/sections/market_list/market_home.tsx
+++ app/src/components/market/sections/market_list/market_home.tsx
@@ -186,7 +186,7 @@ interface Props {
 }
 
 export const MarketHome: React.FC<Props> = (props: Props) => {
-  const { 
+  const {
     categories,
     count,
     currentFilter,
@@ -309,7 +309,7 @@ export const MarketHome: React.FC<Props> = (props: Props) => {
               setCategory('All')
             },
           },
-          ...categories.data.map((item: CategoryDataItem, index) => {
+          ...categories.data.map((item: CategoryDataItem) => {
             return {
               content: <CustomDropdownItem>{item.id}</CustomDropdownItem>,
               onClick: () => {
