c7aa5a88a0d277bb7ee938662b3b747735851306
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-07-16 16:20:52 -0700

    Fix warnings

--- app/src/components/market/common/market_top_details_closed/index.tsx
+++ app/src/components/market/common/market_top_details_closed/index.tsx
@@ -2,7 +2,7 @@ import { BigNumber } from 'ethers/utils'
 import React, { useState } from 'react'
 
 import { LINK_FAQ } from '../../../../common/constants'
-import { formatBigNumber, formatDate, getMarketTitles } from '../../../../util/tools'
+import { formatBigNumber, getMarketTitles } from '../../../../util/tools'
 import { MarketMakerData } from '../../../../util/types'
 import { GridTwoColumns, SubsectionTitleAction, SubsectionTitleWrapper, TitleValue } from '../../../common'
 import { Breaker, SubsectionTitleActionWrapper } from '../common_styled'
@@ -29,7 +29,6 @@ const MarketTopDetailsClosed: React.FC<Props> = (props: Props) => {
     collateralVolume,
     question,
   } = marketMakerData
-  const resolutionFormat = question.resolution ? formatDate(question.resolution) : ''
   const totalVolumeFormat = collateralVolume
     ? `${formatBigNumber(collateralVolume, collateralToken.decimals)} ${collateralToken.symbol}`
     : '-'
