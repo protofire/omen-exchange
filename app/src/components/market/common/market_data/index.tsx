6c8d02bceb82dd81516183ff3018846e37abf4b0
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-08-28 14:48:21 -0700

    Add 24h trade volume

--- app/src/components/market/common/market_data/index.tsx
+++ app/src/components/market/common/market_data/index.tsx
@@ -1,15 +1,18 @@
-import React, { DOMAttributes } from 'react'
+import React, { DOMAttributes, useEffect } from 'react'
 import styled from 'styled-components'
 import { BigNumber } from 'ethers/utils'
 
 import { Token } from '../../../../util/types'
 import { formatBigNumber } from '../../../../util/tools'
+import { useConnectedWeb3Context, useTokens } from '../../../../hooks'
+import { currenciesData } from '../../../common/icons/currencies/currencies_data'
 
 const MarketDataWrapper = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
-  justify-content: space-between;
+  justify-content: space-evenly;
+  margin-bottom: 32px;
 `
 
 const MarketDataItem = styled.div`
@@ -34,6 +37,12 @@ const MarketDataItemBottom = styled.div`
   line-height: 16px;
 `
 
+const MarketDataItemImage = styled.img`
+  max-height: 18px;
+  max-width: 18px;
+  margin-right: 10px;
+`
+
 interface Props extends DOMAttributes<HTMLDivElement> {
   resolutionTimestamp: Date
   dailyVolume: BigNumber
@@ -41,6 +50,13 @@ interface Props extends DOMAttributes<HTMLDivElement> {
 }
 
 export const MarketData: React.FC<Props> = props => {
+  const { resolutionTimestamp, dailyVolume, currency } = props
+
+  const context = useConnectedWeb3Context()
+  const tokens = useTokens(context)
+
+  const currencyIcon = (tokens.filter(token => token.symbol === currency.symbol))[0].image
+
   return (
     <MarketDataWrapper>
       <MarketDataItem>
@@ -61,10 +77,12 @@ export const MarketData: React.FC<Props> = props => {
       </MarketDataItem>
       <MarketDataItem>
         <MarketDataItemTop>
-
+          <MarketDataItemImage src={currencyIcon}></MarketDataItemImage>
+          {/* TODO: Add formatNumber */}
+          {formatBigNumber(dailyVolume, currency.decimals)} {currency.symbol}
         </MarketDataItemTop>
         <MarketDataItemBottom>
-          
+          24h Trade Volume
         </MarketDataItemBottom>
       </MarketDataItem>
     </MarketDataWrapper>
