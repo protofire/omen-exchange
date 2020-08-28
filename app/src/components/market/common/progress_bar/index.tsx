282d4dce9f84dbeb35bc417931e057e10cb29b0c
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-08-28 15:25:27 -0700

    Add progress bar markup

--- app/src/components/market/common/progress_bar/index.tsx
+++ app/src/components/market/common/progress_bar/index.tsx
@@ -1,6 +1,30 @@
 import React, { DOMAttributes } from 'react'
 import styled from 'styled-components'
 
+const ProgressBarWrapper = styled.div`
+
+`
+
+const ProgressBarContainer = styled.div`
+
+`
+
+const ProgressBarDot = styled.div`
+
+`
+
+const ProgressBarLine = styled.div`
+
+`
+
+const ProgressBarTitles = styled.div`
+
+`
+
+const ProgressBarTitle = styled.div`
+
+`
+
 interface Props extends DOMAttributes<HTMLDivElement> {
   state: 'Open' | 'Pending' | 'Finalizing' | 'Ended'
   creationTimestamp: Date
@@ -9,8 +33,30 @@ interface Props extends DOMAttributes<HTMLDivElement> {
 
 export const ProgressBar: React.FC<Props> = props => {
   return (
-    <>
-      Progress Bar
-    </>
+    <ProgressBarWrapper>
+      <ProgressBarContainer>
+        <ProgressBarDot></ProgressBarDot>
+        <ProgressBarLine></ProgressBarLine>
+        <ProgressBarDot></ProgressBarDot>
+        <ProgressBarLine></ProgressBarLine>
+        <ProgressBarDot></ProgressBarDot>
+        <ProgressBarLine></ProgressBarLine>
+        <ProgressBarDot></ProgressBarDot>
+      </ProgressBarContainer>
+      <ProgressBarTitles>
+        <ProgressBarTitle>
+          Open
+        </ProgressBarTitle>
+        <ProgressBarTitle>
+          Finalizing
+        </ProgressBarTitle>
+        <ProgressBarTitle>
+          Arbitration
+        </ProgressBarTitle>
+        <ProgressBarTitle>
+          Closed
+        </ProgressBarTitle>
+      </ProgressBarTitles>
+    </ProgressBarWrapper>
   )
 }
