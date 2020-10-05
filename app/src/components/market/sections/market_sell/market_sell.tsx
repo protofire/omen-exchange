74f1321f47ca733c05791fafd79f3e5abe7b548d
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-10-05 15:45:46 -0700

    Update sell component to include market tabs

--- app/src/components/market/sections/market_sell/market_sell.tsx
+++ app/src/components/market/sections/market_sell/market_sell.tsx
@@ -32,6 +32,7 @@ import { TransactionDetailsRow, ValueStates } from '../../common/transaction_det
 import { ViewCard } from '../../common/view_card'
 import { WalletBalance } from '../../common/wallet_balance'
 import { WarningMessage } from '../../common/warning_message'
+import { MarketNavigation } from '../market_navigation'
 
 const TopCard = styled(ViewCard)`
   padding-bottom: 0;
@@ -188,9 +189,7 @@ const MarketSellWrapper: React.FC<Props> = (props: Props) => {
         <MarketTopDetailsOpen marketMakerData={marketMakerData} title="Sell Shares" />
       </TopCard>
       <BottomCard>
-        <SubsectionTitleWrapper>
-          <SubsectionTitle>Trade Outcome</SubsectionTitle>
-        </SubsectionTitleWrapper>
+        <MarketNavigation activeTab={'SWAP'} marketAddress={marketMakerAddress}></MarketNavigation>
         <OutcomeTable
           balances={balances}
           collateral={collateral}
