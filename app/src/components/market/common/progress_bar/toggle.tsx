fd6ed95dedca111ca99cb7a1aecfc87888517d58
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-10-07 14:04:14 -0700

    Add three dots to toggle button

--- app/src/components/market/common/progress_bar/toggle.tsx
+++ app/src/components/market/common/progress_bar/toggle.tsx
@@ -1,6 +1,20 @@
 import React, { DOMAttributes } from 'react'
 import styled from 'styled-components'
 
+import { Button } from '../../../button'
+
+import { ThreeDots1 } from './img/three_dots_1'
+import { ThreeDots2 } from './img/three_dots_2'
+import { ThreeDots3 } from './img/three_dots_3'
+
+const ToggleButton = styled(Button)`
+  height: 36px;
+`
+
+const ToggleButtonText = styled.span`
+  margin-left: 13px;
+`
+
 interface Props extends DOMAttributes<HTMLDivElement> {
   state: string
   toggleProgressBar: () => void
@@ -17,15 +31,26 @@ export const ProgressBarToggle: React.FC<Props> = props => {
   }
 
   return (
-    <button onClick={toggleProgressBar}>
-      Market
-      {state === marketStates.open
-        ? ' Open'
-        : state === marketStates.finalizing
-        ? ' Finalizing'
-        : state === marketStates.arbitration
-        ? ' Arbitrating'
-        : ' Closed'}
-    </button>
+    <ToggleButton onClick={toggleProgressBar}>
+      {state === marketStates.open ? (
+        <ThreeDots1></ThreeDots1>
+      ) : state === marketStates.finalizing ? (
+        <ThreeDots2></ThreeDots2>
+      ) : state === marketStates.arbitration ? (
+        <ThreeDots2></ThreeDots2>
+      ) : (
+        <ThreeDots3></ThreeDots3>
+      )}
+      <ToggleButtonText>
+        Market
+        {state === marketStates.open
+          ? ' Open'
+          : state === marketStates.finalizing
+          ? ' Finalizing'
+          : state === marketStates.arbitration
+          ? ' Arbitrating'
+          : ' Closed'}
+      </ToggleButtonText>
+    </ToggleButton>
   )
 }
