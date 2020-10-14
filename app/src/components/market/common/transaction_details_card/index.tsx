99fac8178d09bd0d1f6c0e656cb1e89baa7f477c
Jikun Liao <liaojikundever@gmail.com>
2020-10-14 05:27:56 -0400

    fix zigzag issue

--- app/src/components/market/common/transaction_details_card/index.tsx
+++ app/src/components/market/common/transaction_details_card/index.tsx
@@ -9,8 +9,12 @@ const Wrapper = styled.div`
   border-bottom: 0;
   background-image: url('data:image/svg+xml;utf8, <svg viewBox="0 0 200 110" xmlns="http://www.w3.org/2000/svg"><path d="M -15 110 L100 10 L215 110" fill="none" stroke="%23ECEFF1" stroke-width="1" vector-effect="non-scaling-stroke"/></svg>');
   background-position: bottom left;
-  background-size: 7.5px auto;
+  background-size: 2.5% auto;
   background-repeat: repeat-x;
+
+  @media (max-width: ${props => props.theme.themeBreakPoints.md}) {
+    background-size: 2% auto;
+  }
 `
 
 export const TransactionDetailsCard: React.FC = props => {
