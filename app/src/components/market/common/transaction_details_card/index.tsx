09b752d7b4744943fdab211f28993f3cfd6c6a71
Jikun Liao <liaojikundever@gmail.com>
2020-10-14 12:44:15 -0400

    fix comment

--- app/src/components/market/common/transaction_details_card/index.tsx
+++ app/src/components/market/common/transaction_details_card/index.tsx
@@ -5,7 +5,6 @@ const Wrapper = styled.div`
   border: 1px solid ${props => props.theme.borders.borderDisabled};
   background-color: ${props => props.theme.colors.mainBodyBackground};
   padding: 21px;
-  position: relaive;
   border-bottom: 0;
   background-image: url('data:image/svg+xml;utf8, <svg viewBox="0 0 200 110" xmlns="http://www.w3.org/2000/svg"><path d="M -15 110 L100 10 L215 110" fill="none" stroke="%23E8EAF6" stroke-width="1" vector-effect="non-scaling-stroke"/></svg>');
   background-position: bottom left;
