4d2e89b9d86bb523c406899ac4c508d193633124
Thomas Pulber <thomaspulber@Thomass-Mac-Pro.fritz.box>
2020-10-14 14:08:16 +0200

    change zigzag to correct color

--- app/src/components/market/common/transaction_details_card/index.tsx
+++ app/src/components/market/common/transaction_details_card/index.tsx
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   padding: 21px;
   position: relaive;
   border-bottom: 0;
-  background-image: url('data:image/svg+xml;utf8, <svg viewBox="0 0 200 110" xmlns="http://www.w3.org/2000/svg"><path d="M -15 110 L100 10 L215 110" fill="none" stroke="%23ECEFF1" stroke-width="1" vector-effect="non-scaling-stroke"/></svg>');
+  background-image: url('data:image/svg+xml;utf8, <svg viewBox="0 0 200 110" xmlns="http://www.w3.org/2000/svg"><path d="M -15 110 L100 10 L215 110" fill="none" stroke="%23E8EAF6" stroke-width="1" vector-effect="non-scaling-stroke"/></svg>');
   background-position: bottom left;
   background-size: 2.5% auto;
   background-repeat: repeat-x;
