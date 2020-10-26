8922201fe729760874516ff05a2b6f5c7dde7b4f
Jikun Liao <liaojikundever@gmail.com>
2020-10-26 08:59:05 -0400

    New Filter/Search Next/Back button #1216

--- app/src/theme/index.js
+++ app/src/theme/index.js
@@ -76,6 +76,7 @@ const theme = {
   colors: {
     activeListItemBackground: '#fafafa',
     alert: '#E57373',
+    borderColorDar: '#9FA8DA',
     darkGray: '#acacac',
     error: '#fa0000',
     gray: '#b7b7b7',
