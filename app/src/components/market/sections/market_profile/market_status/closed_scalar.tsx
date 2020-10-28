f9bfdcf0c30197d40f9baa602db3320f8e279afd
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-10-28 15:46:44 -0700

    Add wireframe for open and closed scalar market views

--- app/src/components/market/sections/market_profile/market_status/closed_scalar.tsx
+++ app/src/components/market/sections/market_profile/market_status/closed_scalar.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { useState } from 'react'
+import { RouteComponentProps, withRouter } from 'react-router-dom'
 
-const Wrapper = () => {
-  return <p>Closed Scalar</p>
+import { MarketMakerData } from '../../../../../util/types'
+import { MarketTopDetailsOpen } from '../../../common/market_top_details_open'
+import { MarketNavigation } from '../../market_navigation'
+
+import { BottomCard, TopCard } from './open'
+
+interface Props extends RouteComponentProps<Record<string, string | undefined>> {
+  account: Maybe<string>
+  marketMakerData: MarketMakerData
+}
+
+const Wrapper = (props: Props) => {
+  const { marketMakerData } = props
+
+  const { address: marketMakerAddress, isQuestionFinalized, question } = marketMakerData
+
+  const [currentTab, setCurrentTab] = useState('SWAP')
+
+  const marketTabs = {
+    swap: 'SWAP',
+    pool: 'POOL',
+    history: 'HISTORY',
+    verify: 'VERIFY',
+    buy: 'BUY',
+    sell: 'SELL',
+  }
+
+  const switchMarketTab = (newTab: string) => {
+    setCurrentTab(newTab)
+  }
+
+  return (
+    <>
+      <TopCard>
+        <MarketTopDetailsOpen marketMakerData={marketMakerData} />
+      </TopCard>
+      <BottomCard>
+        <MarketNavigation
+          activeTab={currentTab}
+          isQuestionFinalized={isQuestionFinalized}
+          marketAddress={marketMakerAddress}
+          resolutionDate={question.resolution}
+          switchMarketTab={switchMarketTab}
+        ></MarketNavigation>
+      </BottomCard>
+    </>
+  )
 }
 
 export const ClosedScalarMarketDetails = withRouter(Wrapper)
