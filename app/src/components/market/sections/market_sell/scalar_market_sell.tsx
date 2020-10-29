04aece6c4afa23509f27974deb75a24248a5d8d6
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-10-29 15:08:47 -0700

    Create barebones scalar market sell view

--- app/src/components/market/sections/market_sell/scalar_market_sell.tsx
+++ app/src/components/market/sections/market_sell/scalar_market_sell.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+<<<<<<< HEAD
 
 import { MarketMakerData } from '../../../../util/types'
 
 interface Props {
+=======
+import { RouteComponentProps, withRouter } from 'react-router-dom'
+
+import { MarketMakerData } from '../../../../util/types'
+
+interface Props extends RouteComponentProps<any> {
+>>>>>>> bff77617... Create barebones scalar market sell view
   marketMakerData: MarketMakerData
   switchMarketTab: (arg0: string) => void
 }
 
+<<<<<<< HEAD
 export const ScalarMarketSell = (props: Props) => {
   return <p>scalar sell</p>
 }
+=======
+const ScalarMarketSellWrapper: React.FC<Props> = (props: Props) => {
+  return <p>Scalar market sell</p>
+}
+
+export const ScalarMarketSell = withRouter(ScalarMarketSellWrapper)
+>>>>>>> bff77617... Create barebones scalar market sell view
