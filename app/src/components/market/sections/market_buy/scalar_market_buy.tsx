293aa1be4df2013cfe865245b902925c5ad3a67d
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-02 14:18:21 -0800

    Add market scale to scalar market buy

--- app/src/components/market/sections/market_buy/scalar_market_buy.tsx
+++ app/src/components/market/sections/market_buy/scalar_market_buy.tsx
@@ -1,6 +1,9 @@
+import { BigNumber } from 'ethers/utils'
 import React from 'react'
 
 import { MarketMakerData } from '../../../../util/types'
+import { GridTransactionDetails } from '../../common/grid_transaction_details'
+import { MarketScale } from '../../common/market_scale'
 
 interface Props {
   marketMakerData: MarketMakerData
