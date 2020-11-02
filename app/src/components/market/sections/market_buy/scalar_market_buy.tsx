eb7ff843ca3fd3be3d4f54e239129ff90db9da41
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-02 15:54:04 -0800

    Partially complete scalar market buy view

--- app/src/components/market/sections/market_buy/scalar_market_buy.tsx
+++ app/src/components/market/sections/market_buy/scalar_market_buy.tsx
@@ -1,9 +1,20 @@
+import { Zero } from 'ethers/constants'
 import { BigNumber } from 'ethers/utils'
 import React from 'react'
 
-import { MarketMakerData } from '../../../../util/types'
+import { useCollateralBalance, useConnectedWeb3Context } from '../../../../hooks'
+import { formatBigNumber, formatNumber } from '../../../../util/tools'
+import { MarketMakerData, Token } from '../../../../util/types'
+import { ButtonTab } from '../../../button'
+import { BigNumberInput, TextfieldCustomPlaceholder } from '../../../common'
+import { BigNumberInputReturn } from '../../../common/form/big_number_input'
+import { CurrenciesWrapper, GenericError, TabsGrid } from '../../common/common_styled'
+import { CurrencySelector } from '../../common/currency_selector'
 import { GridTransactionDetails } from '../../common/grid_transaction_details'
 import { MarketScale } from '../../common/market_scale'
+import { TransactionDetailsCard } from '../../common/transaction_details_card'
+import { TransactionDetailsLine } from '../../common/transaction_details_line'
+import { TransactionDetailsRow } from '../../common/transaction_details_row'
 
 interface Props {
   marketMakerData: MarketMakerData
