c2fc1df253c6963de48684d081602c069fdd82a4
mtsalenc <mtsalenc@gmail.com>
2020-11-05 18:54:38 -0300

    fix: change on kleros icon breaking UI

--- app/src/components/market/common/arbitrator_icon/index.tsx
+++ app/src/components/market/common/arbitrator_icon/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { IconKleros } from '../../../common/icons'
 
@@ -6,10 +7,19 @@ interface Props {
   id: Maybe<string>
 }
 
+const IconWrapper = styled.div`
+  width: 24px;
+  height: 24px;
+`
+
 export const ArbitratorIcon = ({ id }: Props) => {
   switch (id) {
     case 'kleros':
-      return <IconKleros />
+      return (
+        <IconWrapper>
+          <IconKleros />
+        </IconWrapper>
+      )
     default:
       return null
   }
