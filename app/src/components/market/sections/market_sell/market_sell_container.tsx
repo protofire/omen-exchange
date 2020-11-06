a3f677ad9305adf40977a47d5dfe3ad20e715adf
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-06 15:45:36 -0800

    Conditionally render scalar market sell component

--- app/src/components/market/sections/market_sell/market_sell_container.tsx
+++ app/src/components/market/sections/market_sell/market_sell_container.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { MarketMakerData } from '../../../../util/types'
 
 import { MarketSell } from './market_sell'
+import { ScalarMarketSell } from './scalar_market_sell'
 
 interface Props {
   marketMakerData: MarketMakerData
@@ -13,7 +14,11 @@ interface Props {
 const MarketSellContainer: React.FC<Props> = (props: Props) => {
   const { isScalar, marketMakerData, switchMarketTab } = props
 
-  return <MarketSell isScalar={isScalar} marketMakerData={marketMakerData} switchMarketTab={switchMarketTab} />
+  if (isScalar) {
+    return <ScalarMarketSell marketMakerData={marketMakerData} switchMarketTab={switchMarketTab} />
+  } else {
+    return <MarketSell marketMakerData={marketMakerData} switchMarketTab={switchMarketTab} />
+  }
 }
 
 export { MarketSellContainer }
