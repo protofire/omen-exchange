c6838413cab72b1f114b73a782805903ac75c8e9
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-06 15:55:11 -0800

    Add market sell and buy/cancel buttons

--- app/src/components/market/sections/market_buy/scalar_market_buy.tsx
+++ app/src/components/market/sections/market_buy/scalar_market_buy.tsx
@@ -1,6 +1,16 @@
+import { BigNumber } from 'ethers/utils'
 import React from 'react'
+import styled from 'styled-components'
 
 import { MarketMakerData } from '../../../../util/types'
+import { ButtonContainer } from '../../../button'
+import { ButtonType } from '../../../button/button_styling_types'
+import { MarketBottomNavButton } from '../../common/common_styled'
+import { MarketScale } from '../../common/market_scale'
+
+const StyledButtonContainer = styled(ButtonContainer)`
+  justify-content: space-between;
+`
 
 interface Props {
   marketMakerData: MarketMakerData
@@ -8,5 +18,26 @@ interface Props {
 }
 
 export const ScalarMarketBuy = (props: Props) => {
-  return <p>scalar buy</p>
+  const { marketMakerData, switchMarketTab } = props
+
+  const { outcomeTokenMarginalPrices, question, scalarHigh, scalarLow } = marketMakerData
+
+  return (
+    <>
+      <MarketScale
+        currentPrediction={outcomeTokenMarginalPrices[1]}
+        lowerBound={scalarLow || new BigNumber(0)}
+        startingPointTitle={'Current prediction'}
+        unit={question.title ? question.title.split('[')[1].split(']')[0] : ''}
+        upperBound={scalarHigh || new BigNumber(0)}
+      />
+      <StyledButtonContainer>
+        <MarketBottomNavButton buttonType={ButtonType.secondaryLine} onClick={() => switchMarketTab('SWAP')}>
+          Cancel
+        </MarketBottomNavButton>
+        {/* TODO: Add isBuyDisabled and onClick handler */}
+        <MarketBottomNavButton buttonType={ButtonType.secondaryLine}>Buy Position</MarketBottomNavButton>
+      </StyledButtonContainer>
+    </>
+  )
 }
