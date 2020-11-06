0e0f2a46ed3e5294b3481c7b0d2f33a98f176edc
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-06 15:41:09 -0800

    Conditionally render scalar market pool liquidity component

--- app/src/components/market/sections/market_pooling/market_pool_liquidity_container.tsx
+++ app/src/components/market/sections/market_pooling/market_pool_liquidity_container.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { MarketMakerData } from '../../../../util/types'
 
 import { MarketPoolLiquidity } from './market_pool_liquidity'
+import { ScalarMarketPoolLiquidity } from './scalar_market_pool_liquidity'
 
 interface Props {
   marketMakerData: MarketMakerData
