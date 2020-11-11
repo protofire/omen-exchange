af86401b74d5055b67abadaac1b39e565ab40742
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-11 15:20:45 -0800

    Set ScaleBall to newPrediction value

--- app/src/components/market/sections/market_buy/scalar_market_buy.tsx
+++ app/src/components/market/sections/market_buy/scalar_market_buy.tsx
@@ -132,9 +132,8 @@ export const ScalarMarketBuy = (props: Props) => {
     [new BigNumber(0), 0, amount, new BigNumber(0)],
     calcBuyAmount,
   )
-  console.log(tradedShares)
-  console.log(newPrediction)
-  console.log(debouncedAmount)
+
+  const formattedNewPrediction = (newPrediction - (lowerBound || 0)) / ((upperBound || 0) - (lowerBound || 0))
 
   const formattedNewPrediction =
     newPrediction && (newPrediction - (lowerBound || 0)) / ((upperBound || 0) - (lowerBound || 0))
