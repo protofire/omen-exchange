d64031451364c10495b5a4dd6eb00af529432725
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-18 14:35:25 -0800

    Create barebones sell view

--- app/src/components/market/sections/market_sell/scalar_market_sell.tsx
+++ app/src/components/market/sections/market_sell/scalar_market_sell.tsx
@@ -1,12 +1,247 @@
-import React from 'react'
+import { Zero } from 'ethers/constants'
+import { BigNumber } from 'ethers/utils'
+import React, { useEffect, useMemo, useState } from 'react'
+import ReactTooltip from 'react-tooltip'
+import styled from 'styled-components'
 
-import { MarketMakerData } from '../../../../util/types'
+import { useCollateralBalance, useConnectedWeb3Context, useContracts, useCpk, useCpkAllowance } from '../../../../hooks'
+import { RemoteData } from '../../../../util/remote_data'
+import { formatBigNumber, formatNumber } from '../../../../util/tools'
+import { MarketMakerData, Ternary } from '../../../../util/types'
+import { Button, ButtonContainer, ButtonTab } from '../../../button'
+import { ButtonType } from '../../../button/button_styling_types'
+import { BigNumberInput, TextfieldCustomPlaceholder } from '../../../common'
+import { BigNumberInputReturn } from '../../../common/form/big_number_input'
+import { CurrenciesWrapper, GenericError, TabsGrid } from '../../common/common_styled'
+import { CurrencySelector } from '../../common/currency_selector'
+import { GridTransactionDetails } from '../../common/grid_transaction_details'
+import { SetAllowance } from '../../common/set_allowance'
+import { WarningMessage } from '../../common/warning_message'
+
+const StyledButtonContainer = styled(ButtonContainer)`
+  justify-content: space-between;
+`
 
 interface Props {
+  fetchGraphMarketMakerData: () => Promise<void>
   marketMakerData: MarketMakerData
   switchMarketTab: (arg0: string) => void
 }
 
 export const ScalarMarketSell = (props: Props) => {
-  return <p>scalar sell</p>
+  const { fetchGraphMarketMakerData, marketMakerData, switchMarketTab } = props
+  const context = useConnectedWeb3Context()
+  const cpk = useCpk()
+  const { library: provider } = context
+  const signer = useMemo(() => provider.getSigner(), [provider])
+
+  const {
+    address: marketMakerAddress,
+    balances,
+    collateral,
+    fee,
+    outcomeTokenMarginalPrices,
+    question,
+    scalarHigh,
+    scalarLow,
+  } = marketMakerData
+  const { buildMarketMaker } = useContracts(context)
+  const marketMaker = useMemo(() => buildMarketMaker(marketMakerAddress), [buildMarketMaker, marketMakerAddress])
+
+  const Tabs = {
+    short: 'short',
+    long: 'long',
+  }
+
+  const [amount, setAmount] = useState<BigNumber>(new BigNumber(0))
+  const [amountDisplay, setAmountDisplay] = useState<string>('')
+  const [activeTab, setActiveTab] = useState(Tabs.short)
+  const [isNegativeAmount, setIsNegativeAmount] = useState<boolean>(false)
+
+  const [allowanceFinished, setAllowanceFinished] = useState(false)
+  const { allowance, unlock } = useCpkAllowance(signer, collateral.address)
+
+  const hasEnoughAllowance = RemoteData.mapToTernary(allowance, allowance => allowance.gte(amount))
+  const hasZeroAllowance = RemoteData.mapToTernary(allowance, allowance => allowance.isZero())
+
+  const { collateralBalance: maybeCollateralBalance, fetchCollateralBalance } = useCollateralBalance(
+    collateral,
+    context,
+  )
+  const collateralBalance = maybeCollateralBalance || Zero
+  const walletBalance = formatNumber(formatBigNumber(collateralBalance, collateral.decimals, 5), 5)
+
+  // const lowerBound = scalarLow && Number(formatBigNumber(scalarLow, 18))
+  // const upperBound = scalarHigh && Number(formatBigNumber(scalarHigh, 18))
+
+  useEffect(() => {
+    setIsNegativeAmount(formatBigNumber(amount, collateral.decimals).includes('-'))
+  }, [amount, collateral.decimals])
+
+  // useEffect(() => {
+  //   activeTab === Tabs.short ? setPositionIndex(0) : setPositionIndex(1)
+  // }, [activeTab, Tabs.short])
+
+  const unlockCollateral = async () => {
+    if (!cpk) {
+      return
+    }
+
+    await unlock()
+    setAllowanceFinished(true)
+  }
+
+  // const calcBuyAmount = useMemo(
+  //   () => async (amount: BigNumber): Promise<[BigNumber, number, BigNumber, BigNumber]> => {
+  //     let tradedShares: BigNumber
+  //     let reverseTradedShares: BigNumber
+
+  //     try {
+  //       tradedShares = await marketMaker.calcBuyAmount(amount, positionIndex)
+  //       reverseTradedShares = await marketMaker.calcBuyAmount(amount, positionIndex === 0 ? 1 : 0)
+  //     } catch {
+  //       tradedShares = new BigNumber(0)
+  //       reverseTradedShares = new BigNumber(0)
+  //     }
+
+  //     const balanceAfterTrade = computeBalanceAfterTrade(
+  //       balances.map(b => b.holdings),
+  //       positionIndex,
+  //       amount,
+  //       tradedShares,
+  //     )
+  //     const pricesAfterTrade = MarketMakerService.getActualPrice(balanceAfterTrade)
+
+  //     const newPrediction = pricesAfterTrade[1] * ((upperBound || 0) - (lowerBound || 0)) + (lowerBound || 0)
+
+  //     return [tradedShares, newPrediction, amount, reverseTradedShares]
+  //   },
+  //   [balances, marketMaker, positionIndex, lowerBound, upperBound],
+  // )
+
+  // const [tradedShares, newPrediction, debouncedAmount, reverseTradedShares] = useAsyncDerivedValue(
+  //   amount,
+  //   [new BigNumber(0), 0, amount, new BigNumber(0)],
+  //   calcBuyAmount,
+  // )
+
+  // const formattedNewPrediction =
+  //   newPrediction && (newPrediction - (lowerBound || 0)) / ((upperBound || 0) - (lowerBound || 0))
+
+  // const feePaid = mulBN(debouncedAmount, Number(formatBigNumber(fee, 18, 4)))
+  // const feePercentage = Number(formatBigNumber(fee, 18, 4)) * 100
+
+  // const baseCost = debouncedAmount.sub(feePaid)
+  // const potentialProfit = tradedShares.isZero() ? new BigNumber(0) : tradedShares.sub(amount)
+  // const potentialLossUncapped = reverseTradedShares.isZero()
+  //   ? new BigNumber(0)
+  //   : reverseTradedShares.sub(amount.add(feePaid))
+  // const potentialLoss = reverseTradedShares.isZero()
+  //   ? new BigNumber(0)
+  //   : reverseTradedShares.sub(amount).lt(debouncedAmount)
+  //   ? reverseTradedShares.sub(amount)
+  //   : debouncedAmount
+
+  const currentBalance = `${formatBigNumber(collateralBalance, collateral.decimals, 5)}`
+  // const feeFormatted = `${formatNumber(formatBigNumber(feePaid.mul(-1), collateral.decimals))} ${collateral.symbol}`
+  // const baseCostFormatted = `${formatNumber(formatBigNumber(baseCost, collateral.decimals))} ${collateral.symbol}`
+  // const potentialProfitFormatted = `${formatNumber(formatBigNumber(potentialProfit, collateral.decimals))} ${
+  //   collateral.symbol
+  // }`
+  // const potentialLossFormatted = `${formatNumber(formatBigNumber(potentialLoss, collateral.decimals))} ${
+  //   collateral.symbol
+  // }`
+  // const sharesTotal = formatNumber(formatBigNumber(tradedShares, collateral.decimals))
+  // const total = `${sharesTotal} Shares`
+
+  const showSetAllowance =
+    allowanceFinished || hasZeroAllowance === Ternary.True || hasEnoughAllowance === Ternary.False
+
+  const amountError =
+    maybeCollateralBalance === null
+      ? null
+      : maybeCollateralBalance.isZero() && amount.gt(maybeCollateralBalance)
+      ? `Insufficient balance`
+      : amount.gt(maybeCollateralBalance)
+      ? `Value must be less than or equal to ${currentBalance} ${collateral.symbol}`
+      : null
+
+  // const isBuyDisabled =
+  //   (status !== Status.Ready && status !== Status.Error) ||
+  //   amount.isZero() ||
+  //   hasEnoughAllowance !== Ternary.True ||
+  //   amountError !== null ||
+  //   isNegativeAmount
+
+  return (
+    <>
+      <GridTransactionDetails>
+        <div>
+          <TabsGrid>
+            <ButtonTab active={activeTab === Tabs.short} onClick={() => setActiveTab(Tabs.short)}>
+              Short
+            </ButtonTab>
+            <ButtonTab active={activeTab === Tabs.long} onClick={() => setActiveTab(Tabs.long)}>
+              Long
+            </ButtonTab>
+          </TabsGrid>
+          <CurrenciesWrapper>
+            <CurrencySelector
+              balance={walletBalance}
+              context={context}
+              currency={collateral.address}
+              disabled
+              onSelect={() => null}
+            />
+          </CurrenciesWrapper>
+          <ReactTooltip id="walletBalanceTooltip" />
+          <TextfieldCustomPlaceholder
+            formField={
+              <BigNumberInput
+                decimals={collateral.decimals}
+                name="amount"
+                onChange={(e: BigNumberInputReturn) => {
+                  setAmount(e.value)
+                  setAmountDisplay('')
+                }}
+                style={{ width: 0 }}
+                value={amount}
+                valueToDisplay={amountDisplay}
+              />
+            }
+            onClickMaxButton={() => {
+              setAmount(collateralBalance)
+              setAmountDisplay(walletBalance)
+            }}
+            shouldDisplayMaxButton
+            symbol={collateral.symbol}
+          />
+          {amountError && <GenericError>{amountError}</GenericError>}
+        </div>
+      </GridTransactionDetails>
+      {isNegativeAmount && (
+        <WarningMessage
+          additionalDescription={''}
+          danger={true}
+          description={`Your buy amount should not be negative.`}
+          href={''}
+          hyperlinkDescription={''}
+        />
+      )}
+      {showSetAllowance && (
+        <SetAllowance
+          collateral={collateral}
+          finished={allowanceFinished && RemoteData.is.success(allowance)}
+          loading={RemoteData.is.asking(allowance)}
+          onUnlock={unlockCollateral}
+        />
+      )}
+      <StyledButtonContainer>
+        <Button buttonType={ButtonType.secondaryLine} onClick={() => switchMarketTab('SWAP')}>
+          Cancel
+        </Button>
+        <Button buttonType={ButtonType.primaryAlternative}>Sell Position</Button>
+      </StyledButtonContainer>
+    </>
+  )
 }
