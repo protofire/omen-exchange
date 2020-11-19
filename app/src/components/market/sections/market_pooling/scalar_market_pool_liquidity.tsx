6c8eb39c6a7bf6900413b62c525d6237a7b57f14
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-19 14:40:38 -0800

    Setup left side of transaction details grid

--- app/src/components/market/sections/market_pooling/scalar_market_pool_liquidity.tsx
+++ app/src/components/market/sections/market_pooling/scalar_market_pool_liquidity.tsx
@@ -1,12 +1,147 @@
-import React from 'react'
+import { Zero } from 'ethers/constants'
+import { BigNumber } from 'ethers/utils'
+import React, { useState } from 'react'
 
-import { MarketMakerData } from '../../../../util/types'
+import { useCollateralBalance, useConnectedWeb3Context, useFundingBalance } from '../../../../hooks'
+import { formatBigNumber, formatNumber } from '../../../../util/tools'
+import { MarketMakerData, Token } from '../../../../util/types'
+import { Button, ButtonContainer, ButtonTab } from '../../../button'
+import { BigNumberInput, TextfieldCustomPlaceholder, TitleValue } from '../../../common'
+import { BigNumberInputReturn } from '../../../common/form/big_number_input'
+import { CurrenciesWrapper, GenericError, TabsGrid } from '../../common/common_styled'
+import { CurrencySelector } from '../../common/currency_selector'
+import { GridTransactionDetails } from '../../common/grid_transaction_details'
+import { TokenBalance } from '../../common/token_balance'
+import { TransactionDetailsCard } from '../../common/transaction_details_card'
+import { TransactionDetailsLine } from '../../common/transaction_details_line'
+import { TransactionDetailsRow, ValueStates } from '../../common/transaction_details_row'
 
 interface Props {
   marketMakerData: MarketMakerData
   switchMarketTab: (arg0: string) => void
+  fetchGraphMarketMakerData: () => Promise<void>
+}
+
+enum Tabs {
+  deposit,
+  withdraw,
 }
 
 export const ScalarMarketPoolLiquidity = (props: Props) => {
-  return <p>scalar pool liquidity</p>
+  const { fetchGraphMarketMakerData, marketMakerData } = props
+  const { address: marketMakerAddress, balances, fee, totalEarnings, totalPoolShares, userEarnings } = marketMakerData
+  const context = useConnectedWeb3Context()
+
+  const resolutionDate = marketMakerData.question.resolution.getTime()
+  const currentDate = new Date().getTime()
+  const disableDepositTab = currentDate > resolutionDate
+
+  const [activeTab, setActiveTab] = useState(disableDepositTab ? Tabs.withdraw : Tabs.deposit)
+  const [collateral, setCollateral] = useState<Token>(marketMakerData.collateral)
+  const [amountToFund, setAmountToFund] = useState<Maybe<BigNumber>>(new BigNumber(0))
+  const [amountToFundDisplay, setAmountToFundDisplay] = useState<string>('')
+  const [amountToRemove, setAmountToRemove] = useState<Maybe<BigNumber>>(new BigNumber(0))
+  const [amountToRemoveDisplay, setAmountToRemoveDisplay] = useState<string>('')
+
+  const { collateralBalance: maybeCollateralBalance, fetchCollateralBalance } = useCollateralBalance(
+    collateral,
+    context,
+  )
+  const collateralBalance = maybeCollateralBalance || Zero
+  const { fetchFundingBalance, fundingBalance: maybeFundingBalance } = useFundingBalance(marketMakerAddress, context)
+  const fundingBalance = maybeFundingBalance || Zero
+
+  const walletBalance = formatNumber(formatBigNumber(collateralBalance, collateral.decimals, 5), 5)
+  const sharesBalance = formatBigNumber(fundingBalance, collateral.decimals)
+
+  return (
+    <>
+      <GridTransactionDetails>
+        <div>
+          <TabsGrid>
+            <ButtonTab
+              active={disableDepositTab ? false : activeTab === Tabs.deposit}
+              disabled={disableDepositTab}
+              onClick={() => setActiveTab(Tabs.deposit)}
+            >
+              Deposit
+            </ButtonTab>
+            <ButtonTab
+              active={disableDepositTab ? true : activeTab === Tabs.withdraw}
+              onClick={() => setActiveTab(Tabs.withdraw)}
+            >
+              Withdraw
+            </ButtonTab>
+          </TabsGrid>
+          {activeTab === Tabs.deposit && (
+            <>
+              <CurrenciesWrapper>
+                <CurrencySelector
+                  balance={walletBalance}
+                  context={context}
+                  currency={collateral.address}
+                  disabled
+                  onSelect={(token: Token | null) => {
+                    if (token) {
+                      setCollateral(token)
+                      setAmountToFund(new BigNumber(0))
+                    }
+                  }}
+                />
+              </CurrenciesWrapper>
+
+              <TextfieldCustomPlaceholder
+                formField={
+                  <BigNumberInput
+                    decimals={collateral.decimals}
+                    name="amountToFund"
+                    onChange={(e: BigNumberInputReturn) => {
+                      setAmountToFund(e.value)
+                      setAmountToFundDisplay('')
+                    }}
+                    style={{ width: 0 }}
+                    value={amountToFund}
+                    valueToDisplay={amountToFundDisplay}
+                  />
+                }
+                onClickMaxButton={() => {
+                  setAmountToFund(collateralBalance)
+                  setAmountToFundDisplay(formatBigNumber(collateralBalance, collateral.decimals, 5))
+                }}
+                shouldDisplayMaxButton
+                symbol={collateral.symbol}
+              />
+            </>
+          )}
+          {activeTab === Tabs.withdraw && (
+            <>
+              <TokenBalance text="Pool Tokens" value={formatNumber(sharesBalance)} />
+
+              <TextfieldCustomPlaceholder
+                formField={
+                  <BigNumberInput
+                    decimals={collateral.decimals}
+                    name="amountToRemove"
+                    onChange={(e: BigNumberInputReturn) => {
+                      setAmountToRemove(e.value)
+                      setAmountToRemoveDisplay('')
+                    }}
+                    style={{ width: 0 }}
+                    value={amountToRemove}
+                    valueToDisplay={amountToRemoveDisplay}
+                  />
+                }
+                onClickMaxButton={() => {
+                  setAmountToRemove(fundingBalance)
+                  setAmountToRemoveDisplay(formatBigNumber(fundingBalance, collateral.decimals, 5))
+                }}
+                shouldDisplayMaxButton
+                symbol="Shares"
+              />
+            </>
+          )}
+        </div>
+      </GridTransactionDetails>
+    </>
+  )
 }
