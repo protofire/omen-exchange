8ad15c7d2a1fe13e4db1afe6116f8f1f72b4071e
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-11-25 11:12:04 -0800

    Create useGraphMarketTradeData hook

---
+++ app/src/hooks/useGraphMarketTradeData.tsx
@@ -0,0 +1,70 @@
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { useEffect, useState } from 'react'
+
+import { isObjectEqual } from '../util/tools'
+import { Status } from '../util/types'
+
+const query = gql`
+  query GetMarketTradeData($title: String!, $collateral: Bytes!, $account: Account) {
+    fpmmTrades(where: { title: $title, collateralToken: $collateral, creator: $account }) {
+      title
+      outcomeTokensTraded
+      collateralAmount
+      feeAmount
+      outcomeTokenMarginalPrice
+      oldOutcomeTokenMarginalPrice
+      type
+      outcomeIndex
+    }
+  }
+`
+
+type TradeObject = {
+  title: string
+  outcomeTokensTraded: string
+  collateralAmount: string
+  feeAmount: string
+  outcomeTokenMarginalPrice: string
+  oldOutcomeTokenMarginalPrice: string
+  type: string
+  outcomeIndex: string
+}
+
+type GraphResponseTrades = {
+  tradeObjects: TradeObject[]
+}
+
+type GraphResponse = {
+  trades: Maybe<GraphResponseTrades>
+}
+
+type Result = {
+  trades: TradeObject[]
+  status: Status
+}
+
+export const useGraphMarketTradeData = (title: string, collateral: string): Result => {
+  const [trades, setTrades] = useState<TradeObject[]>([])
+
+  const { data, error, loading } = useQuery<GraphResponse>(query, {
+    notifyOnNetworkStatusChange: true,
+    skip: false,
+    variables: { title: title, collateral: collateral },
+  })
+
+  useEffect(() => {
+    if (!title || !collateral) setTrades([])
+  }, [title, collateral])
+
+  if (data && data.trades && !isObjectEqual(trades, data.trades)) {
+    setTrades(data.trades.tradeObjects)
+  } else if (data && data.trades && !data.trades.tradeObjects.length) {
+    setTrades([])
+  }
+
+  return {
+    trades,
+    status: error ? Status.Error : loading ? Status.Loading : Status.Ready,
+  }
+}
