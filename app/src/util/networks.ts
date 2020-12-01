3a43bf774819b0ea638374f54531f65276a02310
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-12-01 10:12:07 -0800

    Fix subgraph mismatched contracts

--- app/src/util/networks.ts
+++ app/src/util/networks.ts
@@ -104,7 +104,7 @@ const networks: { [K in NetworkId]: Network } = {
     omenTCRListId: 1,
     contracts: {
       realitio: '0x3D00D77ee771405628a4bA4913175EcC095538da',
-      realitioScalarAdapter: '0x3A4A1EFF11b81d3ecEbc92f1Ce902F2C738867b8',
+      realitioScalarAdapter: '0x0e8Db8caD541C0Bf5b611636e81fEc0828bc7902',
       marketMakerFactory: '0x0fB4340432e56c014fa96286de17222822a9281b',
       conditionalTokens: '0x36bede640D19981A82090519bC1626249984c908',
       oracle: '0x17174dC1b62add32a1DE477A357e75b0dcDEed6E',
