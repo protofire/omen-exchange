4fa9a57d749636deef6f556754b0ec0018531e23
Milan Rasovic <ezo@Milans-MacBook-Pro.local>
2020-12-04 20:03:30 +0100

    updated loader

--- app/src/components/market/common/history_section/historySelect.tsx
+++ app/src/components/market/common/history_section/historySelect.tsx
@@ -238,6 +238,7 @@ export const HistorySelect: React.FC<Props> = ({
   }, [])
   const loadNextPage = () => {
     const newPageIndex = pageIndex + pageSize
+    setSharesDataLoader(true)
     if (!paginationNext) {
       return
     }
