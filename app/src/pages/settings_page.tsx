0480f90a699c72af4f89a2dd94ed5e20acee84ea
kboomr <boor2k@gamil.com>
2020-12-21 14:21:26 +0500

    UI Settings

---
+++ app/src/pages/settings_page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { SettingsViewContainer } from '../components'
+
+const SettingsPage: React.FC = () => {
+  return <SettingsViewContainer />
+}
+
+export { SettingsPage }
