57fdc4aca30244f69e2b870fce3bd9a17d270698
superarius <superarius@gmail.com>
2020-12-24 10:05:26 -0500

    use gelato mainnet subgraph

--- app/src/common/constants.ts
+++ app/src/common/constants.ts
@@ -51,8 +51,8 @@ export const TRADING_FEE_OPTIONS = [
   '5.00',
 ]
 
-export const GRAPH_MAINNET_HTTP = 'https://api.thegraph.com/subgraphs/name/protofire/omen'
-export const GRAPH_MAINNET_WS = 'wss://api.thegraph.com/subgraphs/name/protofire/omen'
+export const GRAPH_MAINNET_HTTP = 'https://api.thegraph.com/subgraphs/name/hilmarx/gelato-omen-mainnet'
+export const GRAPH_MAINNET_WS = 'wss://api.thegraph.com/subgraphs/name/hilmarx/gelato-omen-mainnet'
 export const GRAPH_RINKEBY_HTTP = 'https://api.thegraph.com/subgraphs/name/hilmarx/omen-gelato'
 export const GRAPH_RINKEBY_WS = 'wss://api.thegraph.com/subgraphs/name/hilmarx/omen-gelato'
 export const GRAPH_SOKOL_HTTP = 'https://api.thegraph.com/subgraphs/name/protofire/omen-sokol'
