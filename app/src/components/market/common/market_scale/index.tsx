c1d60ea1598b94e0ec9614b774699854af2b62de
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-01-07 15:24:20 -0800

    Set payout/profit values based on redeem amounts

--- app/src/components/market/common/market_scale/index.tsx
+++ app/src/components/market/common/market_scale/index.tsx
@@ -330,50 +330,16 @@ export const MarketScale: React.FC<Props> = (props: Props) => {
   }, [newPrediction, currentPrediction, lowerBoundNumber, startingPointNumber, upperBoundNumber])
 
   useEffect(() => {
-    // If taking a long position
     if (long) {
-      // If the value selected by the slider is > the new prediction number
-      if (scaleValuePrediction > newPredictionNumber) {
-        // Determine the percentage distance from the new prediction number to the upper bound
-        const positionValue = (scaleValuePrediction - newPredictionNumber) / (upperBoundNumber - newPredictionNumber)
-        // Calculate profit amount given how close it is to the upper bound, i.e. how close it is to max profit
-        const profit = positionValue * ((amountSharesNumber || 0) - (amountNumber || 0)) - (feeNumber || 0)
-        // Calculate total payout by adding profit to amount
-        setYourPayout(profit + (amountNumber || 0))
-        // Return profit amount
-        setProfitLoss(profit)
-      } else {
-        // Determine the percentage distance from the new prediction number to the lower bound as a negative value
-        const positionValue = -(scaleValuePrediction - newPredictionNumber) / (lowerBoundNumber - newPredictionNumber)
-        // Calculate loss amount given how close it is to lower bound, i.e. how close it is to max loss
-        const loss = positionValue * (amountNumber || 0) - (feeNumber || 0)
-        // Calculate total payout by adding loss to amount
-        setYourPayout((amountNumber || 0) + loss < 0 ? 0 : (amountNumber || 0) + loss)
-        // Return loss amount
-        setProfitLoss(loss < -(amountNumber || 0) ? -(amountNumber || 0) : loss)
-      }
-      // If taking a short position
+      // Calculate total payout by mulitplying shares amount by scale position
+      setYourPayout((amountSharesNumber || 0) * (Number(scaleBall?.value) / 100))
+      // Calculate profit by subtracting amount paid from payout
+      setProfitLoss((amountSharesNumber || 0) * (Number(scaleBall?.value) / 100) - (amountNumber || 0))
     } else {
-      // If the value selected by the slider is < the new prediction number
-      if (scaleValuePrediction <= newPredictionNumber) {
-        // Determine the percentage distance from the new prediction number to the lower bound
-        const positionValue = (newPredictionNumber - scaleValuePrediction) / (newPredictionNumber - lowerBoundNumber)
-        // Calculate profit amount given how close it is to the lower bound, i.e. how close it is to max profit
-        const profit = positionValue * ((amountSharesNumber || 0) - (amountNumber || 0)) - (feeNumber || 0)
-        // Calculate total payout by adding profit to amount
-        setYourPayout(profit + (amountNumber || 0))
-        // Return profit amount
-        setProfitLoss(profit)
-      } else {
-        // Determine the percentage distance from the new prediction number to the upper bound as a negative value
-        const positionValue = -(scaleValuePrediction - newPredictionNumber) / (upperBoundNumber - newPredictionNumber)
-        // Calculate loss amount given how close it is to upper bound, i.e. how close it is to max loss
-        const loss = positionValue * (amountNumber || 0) - (feeNumber || 0)
-        // Calculate total payout by adding loss to amount
-        setYourPayout((amountNumber || 0) + loss < 0 ? 0 : (amountNumber || 0) + loss)
-        // Return loss amount
-        setProfitLoss(loss < -(amountNumber || 0) ? -(amountNumber || 0) : loss)
-      }
+      // Calculate total payout by mulitplying shares amount by scale position
+      setYourPayout((amountSharesNumber || 0) * (1 - Number(scaleBall?.value) / 100))
+      // Calculate profit by subtracting amount paid from payout
+      setProfitLoss((amountSharesNumber || 0) * (1 - Number(scaleBall?.value) / 100) - (amountNumber || 0))
     }
   }, [
     scaleValuePrediction,
