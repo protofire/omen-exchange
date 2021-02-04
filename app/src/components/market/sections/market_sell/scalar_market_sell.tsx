ce626d3eb15ff6357b54551a07d3e1e337837734
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-02-04 14:04:06 -0800

    Include calcXValue in scalar market sell

--- app/src/components/market/sections/market_sell/scalar_market_sell.tsx
+++ app/src/components/market/sections/market_sell/scalar_market_sell.tsx
@@ -1,5 +1,5 @@
 import { Zero } from 'ethers/constants'
-import { BigNumber } from 'ethers/utils'
+import { BigNumber, parseUnits } from 'ethers/utils'
 import React, { useEffect, useMemo, useState } from 'react'
 import ReactTooltip from 'react-tooltip'
 import styled from 'styled-components'
@@ -9,6 +9,7 @@ import { CPKService, MarketMakerService } from '../../../../services'
 import { getLogger } from '../../../../util/logger'
 import {
   calcSellAmountInCollateral,
+  calcXValue,
   computeBalanceAfterTrade,
   formatBigNumber,
   formatNumber,
@@ -151,7 +152,13 @@ export const ScalarMarketSell = (props: Props) => {
   )
 
   const formattedNewPrediction =
-    newPrediction && (newPrediction - (lowerBound || 0)) / ((upperBound || 0) - (lowerBound || 0))
+    newPrediction &&
+    calcXValue(
+      parseUnits(newPrediction.toString(), 18),
+      scalarLow || new BigNumber(0),
+      scalarHigh || new BigNumber(0),
+      18,
+    ) / 100
 
   const finish = async () => {
     const outcomeIndex = positionIndex
