41cdb292161e7529b8a67dd477a26df073f5eaca
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-02-04 16:10:33 -0800

    Fix isScalarMarket test

--- app/src/util/tools.spec.ts
+++ app/src/util/tools.spec.ts
@@ -3,6 +3,7 @@ import Big from 'big.js'
 import { BigNumber, bigNumberify } from 'ethers/utils'
 
 import { REALITIO_SCALAR_ADAPTER_ADDRESS, REALITIO_SCALAR_ADAPTER_ADDRESS_RINKEBY } from '../common/constants'
+import { getContractAddress } from './networks'
 
 import {
   calcAddFundingSendAmounts,
@@ -484,10 +485,10 @@ describe('tools', () => {
 
   describe('isScalarMarket', () => {
     const testCases: [[string, number], boolean][] = [
-      [[REALITIO_SCALAR_ADAPTER_ADDRESS.toLowerCase(), 1], true],
-      [[REALITIO_SCALAR_ADAPTER_ADDRESS_RINKEBY.toLowerCase(), 4], true],
-      [[REALITIO_SCALAR_ADAPTER_ADDRESS.toLowerCase(), 4], false],
-      [[REALITIO_SCALAR_ADAPTER_ADDRESS_RINKEBY.toLowerCase(), 1], false],
+      [[getContractAddress(1, 'realitioScalarAdapter').toLowerCase(), 1], true],
+      [[getContractAddress(4, 'realitioScalarAdapter').toLowerCase(), 4], true],
+      [[getContractAddress(4, 'realitio').toLowerCase(), 4], false],
+      [[getContractAddress(1, 'realitio').toLowerCase(), 1], false],
       [['Incorrect address', 1], false],
     ]
     for (const [[oracle, networkId], result] of testCases) {
