5591778904d18aee6750d721fde7f1a90faa2a30
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-02-26 11:23:54 -0800

    Add modal navigation

---
+++ app/src/components/modal/common_styled/index.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const ContentWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+`
+
+export const ModalNavigation = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 14px;
+`
--- app/src/components/modal/modal_your_connection/index.tsx
+++ app/src/components/modal/modal_your_connection/index.tsx
@@ -2,6 +2,16 @@ import React, { HTMLAttributes } from 'react'
 import Modal from 'react-modal'
 import styled, { withTheme } from 'styled-components'
 
+import { IconClose } from '../../common/icons'
+import { ContentWrapper, ModalNavigation } from '../common_styled'
+
+const ModalTitle = styled.p`
+  font-size: 16px;
+  color: ${props => props.theme.colors.textColorDark};
+  font-weight: 500;
+  margin: 0;
+`
+
 interface Props extends HTMLAttributes<HTMLDivElement> {
   isOpen: boolean
   onClose: () => void
@@ -16,12 +26,14 @@ export const ModalYourConnection = (props: Props) => {
   }, [])
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      shouldCloseOnOverlayClick={true}
-      style={theme.connectionModal}
-    ></Modal>
+    <Modal isOpen={isOpen} onRequestClose={onClose} shouldCloseOnOverlayClick={true} style={theme.connectionModal}>
+      <ContentWrapper>
+        <ModalNavigation>
+          <ModalTitle>Your Connection</ModalTitle>
+          <IconClose hoverEffect={true} />
+        </ModalNavigation>
+      </ContentWrapper>
+    </Modal>
   )
 }
 
