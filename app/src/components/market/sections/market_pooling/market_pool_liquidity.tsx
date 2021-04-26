d03e3a01f4e51a7e465727e8a136054af3e3ff72
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-04-26 15:16:16 -0700

    Add cpk.addFundingAndStake

--- app/src/components/market/sections/market_pooling/market_pool_liquidity.tsx
+++ app/src/components/market/sections/market_pooling/market_pool_liquidity.tsx
@@ -318,6 +318,28 @@ const MarketPoolLiquidityWrapper: React.FC<Props> = (props: Props) => {
     }
   }
 
+  const addFundingAndStake = async () => {
+    if (!cpk) {
+      return
+    }
+
+    let useBaseToken = false
+    if (displayCollateral.address !== collateral.address && collateral.symbol.toLowerCase() in CompoundTokenType) {
+      useBaseToken = true
+    }
+
+    await cpk.addFundingAndStake({
+      amount: amountToFundNormalized || Zero,
+      campaignAddress: liquidityMiningCampaign?.id || '',
+      compoundService,
+      collateral,
+      marketMaker,
+      setTxHash,
+      setTxState,
+      useBaseToken,
+    })
+  }
+
   const removeFunding = async () => {
     try {
       if (!cpk) {
@@ -900,6 +922,7 @@ const MarketPoolLiquidityWrapper: React.FC<Props> = (props: Props) => {
             Withdraw
           </Button>
         )}
+        <Button onClick={addFundingAndStake}>Deposit and Stake</Button>
         <Button onClick={stake}>Stake</Button>
         <Button onClick={withdraw}>Withdraw</Button>
         <Button onClick={claim}>Claim</Button>
