efcbc27e6add0bf6b9d4178545cc2087943a335c
Eric Rice <ericmichaelrice@protonmail.com>
2021-05-03 15:42:04 -0400

    add displayCollateral to UI message

--- app/src/components/market/sections/market_pooling/scalar_market_pool_liquidity.tsx
+++ app/src/components/market/sections/market_pooling/scalar_market_pool_liquidity.tsx
@@ -309,7 +309,7 @@ export const ScalarMarketPoolLiquidity = (props: Props) => {
 
       setTxState(TransactionStep.waitingConfirmation)
       setIsTransactionModalOpen(true)
-      let fundsAmount = formatBigNumber(depositedTokensTotal, collateral.decimals)
+      let fundsAmount = formatBigNumber(depositedTokensTotal, collateral.decimals, collateral.decimals)
       if (
         compoundService &&
         collateralSymbol in CompoundTokenType &&
@@ -322,7 +322,7 @@ export const ScalarMarketPoolLiquidity = (props: Props) => {
         fundsAmount = formatBigNumber(displayDepositedTokensTotal || Zero, displayCollateral.decimals)
       }
 
-      setMessage(`Withdrawing funds: ${fundsAmount} ${displayCollateral.symbol}...`)
+      setMessage(`Withdrawing funds: ${formatNumber(fundsAmount)} ${displayCollateral.symbol}...`)
 
       const collateralAddress = await marketMaker.getCollateralToken()
       const conditionId = await marketMaker.getConditionId()
@@ -358,7 +358,7 @@ export const ScalarMarketPoolLiquidity = (props: Props) => {
       setAmountToRemove(null)
       setAmountToRemoveDisplay('')
       setAmountToRemoveNormalized(null)
-      setMessage(`Successfully withdrew ${fundsAmount} ${collateral.symbol}`)
+      setMessage(`Successfully withdrew ${formatNumber(fundsAmount)} ${displayCollateral.symbol}`)
     } catch (err) {
       setTxState(TransactionStep.error)
       setMessage(`Error trying to withdraw funds.`)
