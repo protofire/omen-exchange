38690dfa1785b86b5ac5cf6b9cb43b5ab2ecad4c
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-05-07 16:11:05 -0700

    Conditionally unstakeClaimAndWithdraw on scalar markets

--- app/src/components/market/sections/market_pooling/scalar_market_pool_liquidity.tsx
+++ app/src/components/market/sections/market_pooling/scalar_market_pool_liquidity.tsx
@@ -450,6 +450,68 @@ export const ScalarMarketPoolLiquidity = (props: Props) => {
     }
   }
 
+  const unstakeClaimAndRemoveFunding = async () => {
+    try {
+      if (!cpk) {
+        return
+      }
+      if (!liquidityMiningCampaign) {
+        throw 'No liquidity mining campaign'
+      }
+
+      const fundsAmount = formatBigNumber(depositedTokensTotal, collateral.decimals, collateral.decimals)
+      setMessage(`Unstaking and withdrawing funds: ${formatNumber(fundsAmount)} ${displayCollateral.symbol}...`)
+
+      const collateralAddress = await marketMaker.getCollateralToken()
+      const conditionId = await marketMaker.getConditionId()
+
+      setTxState(TransactionStep.waitingConfirmation)
+      setIsTransactionProcessing(true)
+      setIsTransactionModalOpen(true)
+
+      await cpk.unstakeClaimAndWithdraw({
+        amountToMerge: depositedTokens,
+        campaignAddress: liquidityMiningCampaign.id,
+        collateralAddress,
+        conditionId,
+        conditionalTokens,
+        compoundService,
+        earnings: userEarnings,
+        marketMaker,
+        outcomesCount: balances.length,
+        setTxHash,
+        setTxState,
+        sharesToBurn: amountToRemove || Zero,
+        useBaseToken: false, // Not using base token for staking
+      })
+
+      await fetchGraphMarketMakerData()
+      await fetchFundingBalance()
+      await fetchCollateralBalance()
+      await fetchBalances()
+      await fetchStakingData()
+
+      setAmountToRemove(null)
+      setAmountToRemoveDisplay('')
+      setAmountToRemoveNormalized(null)
+      setMessage(`Successfully withdrew ${formatNumber(fundsAmount)} ${displayCollateral.symbol}`)
+      setIsTransactionProcessing(false)
+    } catch (err) {
+      setTxState(TransactionStep.error)
+      setMessage(`Error trying to unstaked and withdraw funds.`)
+      logger.error(`${message} - ${err.message}`)
+      setIsTransactionProcessing(false)
+    }
+  }
+
+  const withdraw = () => {
+    if (liquidityMiningCampaign) {
+      unstakeClaimAndRemoveFunding()
+    } else {
+      removeFunding()
+    }
+  }
+
   const { liquidityMiningCampaigns } = useGraphLiquidityMiningCampaigns()
 
   useEffect(() => {
@@ -956,7 +1018,7 @@ export const ScalarMarketPoolLiquidity = (props: Props) => {
           </Button>
         )}
         {activeTab === Tabs.withdraw && (
-          <Button buttonType={ButtonType.primaryAlternative} disabled={disableWithdrawButton} onClick={removeFunding}>
+          <Button buttonType={ButtonType.primaryAlternative} disabled={disableWithdrawButton} onClick={withdraw}>
             Withdraw
           </Button>
         )}
