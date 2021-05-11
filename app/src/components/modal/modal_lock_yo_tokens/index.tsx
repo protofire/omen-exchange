f7736f18f64b6381a6e07a4cc5f132c2d3a24754
My Violet <milanrasovic.f@gmail.com>
2021-05-11 15:08:01 +0200

    started working on lock omen token modal

--- app/src/components/modal/modal_lock_yo_tokens/index.tsx
+++ app/src/components/modal/modal_lock_yo_tokens/index.tsx
@@ -1,9 +1,16 @@
-import React, { HTMLAttributes } from 'react'
+import { Zero } from 'ethers/constants'
+import { BigNumber } from 'ethers/utils'
+import React, { HTMLAttributes, useState } from 'react'
 import Modal from 'react-modal'
 import styled, { withTheme } from 'styled-components'
 
+import { STANDARD_DECIMALS } from '../../../common/constants'
+import { formatBigNumber } from '../../../util/tools'
+import { ExchangeCurrency, ExchangeType } from '../../../util/types'
 import { Button } from '../../button/button'
 import { ButtonType } from '../../button/button_styling_types'
+import { TextfieldCustomPlaceholder } from '../../common'
+import { BigNumberInput, BigNumberInputReturn } from '../../common/form/big_number_input'
 import { IconClose } from '../../common/icons'
 import { ContentWrapper, ModalNavigation } from '../common_styled'
 
@@ -35,12 +42,15 @@ const ButtonSection = styled.div`
   margin-top: 28px;
 `
 const ButtonsLockUnlock = styled(Button)`
-  width: 50%;
+  width: 100%;
 `
 
 const ModalLockTokens = (props: Props) => {
   const { isOpen, onClose, theme } = props
-
+  const [isLockAmountOpen, setIsLockAmountOpen] = useState<boolean>(false)
+  const [displayLockAmount, setDisplayLockAmount] = useState<BigNumber>(Zero)
+  const [amountToDisplay, setAmountToDisplay] = useState<string>('')
+  console.log(isLockAmountOpen)
   return (
     <Modal isOpen={isOpen} style={theme.fluidHeightModal}>
       <ContentWrapper>
@@ -61,21 +71,51 @@ const ModalLockTokens = (props: Props) => {
             <DarkDataItem>450 OMN</DarkDataItem>
           </DataRow>
           <DataRow>
-            <LightDataItem>Omen Account</LightDataItem>
-            <DarkDataItem>450 OMN</DarkDataItem>
+            <LightDataItem>Locked in Guild</LightDataItem>
+            <DarkDataItem>450 000 OMN</DarkDataItem>
+          </DataRow>
+          {isLockAmountOpen && (
+            <TextfieldCustomPlaceholder
+              formField={
+                <BigNumberInput
+                  decimals={STANDARD_DECIMALS}
+                  name="amount"
+                  onChange={(e: BigNumberInputReturn) => {
+                    setDisplayLockAmount(e.value)
+                    setAmountToDisplay('')
+                  }}
+                  value={displayLockAmount}
+                  valueToDisplay={amountToDisplay}
+                />
+              }
+              onClickMaxButton={() => {
+                const maxBalance = new BigNumber('1')
+                setDisplayLockAmount(maxBalance)
+                setAmountToDisplay(formatBigNumber(maxBalance, STANDARD_DECIMALS, 5))
+              }}
+              shouldDisplayMaxButton={true}
+              symbol={'OMN'}
+            />
+          )}
+
+          <DataRow>
+            <LightDataItem>Vote Weight</LightDataItem>
+            <DarkDataItem>0.60%</DarkDataItem>
           </DataRow>
           <DataRow>
-            <LightDataItem>Omen Account</LightDataItem>
-            <DarkDataItem>450 OMN</DarkDataItem>
-          </DataRow>
-          <DataRow>
-            <LightDataItem>Omen Account</LightDataItem>
-            <DarkDataItem>450 OMN</DarkDataItem>
+            <LightDataItem>Unlock Date</LightDataItem>
+            <DarkDataItem>March 31rd,2021-2:32 UTC</DarkDataItem>
           </DataRow>
         </ModalMain>
         <ButtonSection>
-          <ButtonsLockUnlock buttonType={ButtonType.primaryLine}>Unlock Omen</ButtonsLockUnlock>
-          <ButtonsLockUnlock buttonType={ButtonType.primaryAlternative}>Lock OMN</ButtonsLockUnlock>
+          {!isLockAmountOpen && <ButtonsLockUnlock buttonType={ButtonType.primaryLine}>Unlock Omen</ButtonsLockUnlock>}
+
+          <ButtonsLockUnlock
+            buttonType={ButtonType.primaryAlternative}
+            onClick={() => setIsLockAmountOpen(!isLockAmountOpen)}
+          >
+            Lock OMN
+          </ButtonsLockUnlock>
         </ButtonSection>
       </ContentWrapper>
     </Modal>
