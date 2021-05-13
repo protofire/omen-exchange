88ace3fdaea466a01f757f685ef763f57c9ea286
Eric Rice <ericmichaelrice@protonmail.com>
2021-05-13 02:42:08 -0400

    if currentAPR render this, else render current data as column + reward column data

--- app/src/components/market/sections/market_pooling/user_pool_data.tsx
+++ app/src/components/market/sections/market_pooling/user_pool_data.tsx
@@ -139,78 +139,84 @@ export const UserPoolData: React.FC<Props> = (props: Props) => {
   {
     console.log('total user liquidity' + totalUserLiquidity)
   }
-  currentApr == 0 && (
-    <>
-      <Border />
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <PoolOverview>Pool Overview</PoolOverview>
-        {currentApr == 0 && (
-          <PoolOverview style={{ marginLeft: 'auto', width: '253px' }}>Liquidity Rewards</PoolOverview>
-        )}
-      </div>
+  if (currentApr)
+    return (
+      <>
+        <Border />
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <PoolOverview>Pool Overview</PoolOverview>
+          {currentApr > 0 && (
+            <PoolOverview style={{ marginLeft: 'auto', width: '253px' }}>Liquidity Rewards</PoolOverview>
+          )}
+        </div>
 
+        <UserDataWrapper>
+          <ColumnWrapper>
+            <UserDataColumn>
+              <UserDataTitleValue
+                title="Your Liquidity"
+                value={`${formatNumber(
+                  formatBigNumber(displayUserLiquidity, baseCollateral.decimals, baseCollateral.decimals),
+                )} ${baseCollateral.symbol}`}
+              />
+
+              <UserDataTitleValue
+                title="Total Liquidity"
+                value={`${formatNumber(
+                  formatBigNumber(displayPoolTokens, baseCollateral.decimals, baseCollateral.decimals),
+                )}`}
+              />
+            </UserDataColumn>
+          </ColumnWrapper>
+          <ColumnWrapper style={{ marginLeft: 'auto' }}>
+            <UserDataColumn>
+              <UserDataTitleValue
+                state={userEarnings.gt(0) ? ValueStates.success : undefined}
+                title="Your Earnings"
+                value={`${displayUserEarnings.gt(0) ? '+' : ''}${formatNumber(
+                  formatBigNumber(displayUserEarnings, baseCollateral.decimals, baseCollateral.decimals),
+                )} ${baseCollateral.symbol}`}
+              />
+              <UserDataTitleValue
+                state={displayTotalEarnings.gt(0) ? ValueStates.success : undefined}
+                title="Total Earnings"
+                value={`${displayTotalEarnings.gt(0) ? '+' : ''}${formatNumber(
+                  formatBigNumber(displayTotalEarnings, baseCollateral.decimals, baseCollateral.decimals),
+                )} ${baseCollateral.symbol}`}
+              />
+            </UserDataColumn>
+          </ColumnWrapper>
+        </UserDataWrapper>
+      </>
+    )
+  else
+    return (
       <UserDataWrapper>
         <ColumnWrapper>
           <UserDataColumn>
             <UserDataTitleValue
-              title="Your Liquidity"
-              value={`${formatNumber(
-                formatBigNumber(displayUserLiquidity, baseCollateral.decimals, baseCollateral.decimals),
-              )} ${baseCollateral.symbol}`}
+              state={currentApr > 0 ? ValueStates.success : undefined}
+              title="Current APY"
+              value={`${formatNumber(currentApr.toString())}%`}
+            />
+            <UserDataTitleValue
+              state={remainingRewards > 0 ? ValueStates.success : undefined}
+              title="Rewards left"
+              value={`${formatNumber(remainingRewards.toString())} OMN`}
             />
 
             <UserDataTitleValue
-              title="Total Liquidity"
-              value={`${formatNumber(
-                formatBigNumber(displayPoolTokens, baseCollateral.decimals, baseCollateral.decimals),
-              )}`}
-            />
-          </UserDataColumn>
-        </ColumnWrapper>
-        <ColumnWrapper style={{ marginLeft: 'auto' }}>
-          <UserDataColumn>
-            <UserDataTitleValue
-              state={userEarnings.gt(0) ? ValueStates.success : undefined}
-              title="Your Earnings"
-              value={`${displayUserEarnings.gt(0) ? '+' : ''}${formatNumber(
-                formatBigNumber(displayUserEarnings, baseCollateral.decimals, baseCollateral.decimals),
-              )} ${baseCollateral.symbol}`}
+              state={earnedRewards > 0 ? ValueStates.success : undefined}
+              title="Your Rewards"
+              value={`${formatNumber(earnedRewards.toString())} OMN`}
             />
             <UserDataTitleValue
-              state={displayTotalEarnings.gt(0) ? ValueStates.success : undefined}
-              title="Total Earnings"
-              value={`${displayTotalEarnings.gt(0) ? '+' : ''}${formatNumber(
-                formatBigNumber(displayTotalEarnings, baseCollateral.decimals, baseCollateral.decimals),
-              )} ${baseCollateral.symbol}`}
+              state={totalRewards > 0 ? ValueStates.success : undefined}
+              title="Total Rewards"
+              value={`${formatNumber(totalRewards.toString())} OMN`}
             />
           </UserDataColumn>
         </ColumnWrapper>
       </UserDataWrapper>
-    </>
-  )
-  currentApr > 0 && (
-    <UserDataColumn>
-      <UserDataTitleValue
-        state={currentApr > 0 ? ValueStates.success : undefined}
-        title="Current APY"
-        value={`${formatNumber(currentApr.toString())}%`}
-      />
-      <UserDataTitleValue
-        state={remainingRewards > 0 ? ValueStates.success : undefined}
-        title="Rewards left"
-        value={`${formatNumber(remainingRewards.toString())} OMN`}
-      />
-
-      <UserDataTitleValue
-        state={earnedRewards > 0 ? ValueStates.success : undefined}
-        title="Your Rewards"
-        value={`${formatNumber(earnedRewards.toString())} OMN`}
-      />
-      <UserDataTitleValue
-        state={totalRewards > 0 ? ValueStates.success : undefined}
-        title="Total Rewards"
-        value={`${formatNumber(totalRewards.toString())} OMN`}
-      />
-    </UserDataColumn>
-  )
+    )
 }
