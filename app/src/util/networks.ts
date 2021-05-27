25e1c42e4d0ea73626cb4a99b5e032e9177c610a
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-05-27 15:56:41 -0700

    Update staking rewards factory address

--- app/src/util/networks.ts
+++ app/src/util/networks.ts
@@ -172,7 +172,7 @@ const networks: { [K in NetworkId]: Network } = {
       klerosTCR: '0x0000000000000000000000000000000000000000',
       dxTCR: '0x03165DF66d9448E45c2f5137486af3E7e752a352',
       omenVerifiedMarkets: '0x3b29096b7ab49428923d902cEC3dFEaa49993234',
-      stakingRewardsFactory: '0xFc3745C6909355EbBDDFa58181EB50Fd5cEF3eEd',
+      stakingRewardsFactory: '0x9A66cd359Fc4c8d1a3B124E8A2508b4311493D87',
     },
     cpk: {
       masterCopyAddress: '0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
