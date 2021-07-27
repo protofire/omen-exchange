954a16cb8e847bbda957f9aa9cb10f11e524f461
Francesco <vaubanf@gmail.com>
2021-07-27 10:04:39 +0200

    Fix: Fix outcomes colors proper rendering

--- app/src/theme/utils.ts
+++ app/src/theme/utils.ts
@@ -139,12 +139,12 @@ export const getOutcomeColor = (index: number): Record<string, any> => {
   const localTheme: Record<string, any> = outcomes
   const defaultReturn = { darker: '#000', medium: '#999' }
 
-  if (!localTheme || !localTheme['outcomes'] || !localTheme['outcomes']['colors']) {
+  if (!localTheme || !localTheme['colors']) {
     logger.error('Error when using theme or theme.outcomes')
     return defaultReturn
   }
 
-  const colors = localTheme.outcomes.colors
+  const colors = localTheme.colors
 
   return colors[index % colors.length]
 }
