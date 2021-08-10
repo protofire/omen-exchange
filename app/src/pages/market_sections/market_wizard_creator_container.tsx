f1f5d02e82a2918f121105caa20d1c27b4d59f45
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-08-10 13:42:15 -0700

    feat: only create v2 markets

--- app/src/pages/market_sections/market_wizard_creator_container.tsx
+++ app/src/pages/market_sections/market_wizard_creator_container.tsx
@@ -21,7 +21,7 @@ const MarketWizardCreatorContainer: FC = () => {
   const history = useHistory()
 
   const [isModalOpen, setModalState] = useState(false)
-  const { conditionalTokens, marketMakerFactory, realitio } = useContracts(context)
+  const { conditionalTokens, marketMakerFactoryV2, realitio } = useContracts(context)
 
   const [marketCreationStatus, setMarketCreationStatus] = useState<MarketCreationStatus>(MarketCreationStatus.ready())
   const [marketMakerAddress, setMarketMakerAddress] = useState<string | null>(null)
@@ -59,7 +59,7 @@ const MarketWizardCreatorContainer: FC = () => {
             marketData,
             conditionalTokens,
             realitio,
-            marketMakerFactory,
+            marketMakerFactory: marketMakerFactoryV2,
           })
           await waitUntilContractDeployed(provider, marketMakerAddress)
           await fetchBalances()
@@ -72,7 +72,7 @@ const MarketWizardCreatorContainer: FC = () => {
             marketData,
             conditionalTokens,
             realitio,
-            marketMakerFactory,
+            marketMakerFactory: marketMakerFactoryV2,
           })
           await waitUntilContractDeployed(provider, marketMakerAddress)
           await fetchBalances()
