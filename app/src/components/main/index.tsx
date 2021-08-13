12c26d27351673d36da1a1e65fe09bab283281a3
My Violet <milanrasovic.f@gmail.com>
2021-08-13 15:37:28 +0200

    feat: Added view components and dummy date to be used

--- app/src/components/main/index.tsx
+++ app/src/components/main/index.tsx
@@ -4,6 +4,7 @@ import { Redirect, Route, HashRouter as Router, Switch } from 'react-router-dom'
 import { useWeb3Context } from 'web3-react'
 
 import { DOCUMENT_TITLE } from '../../common/constants'
+import { ProposalDetailsPage } from '../../pages/guild/proposal_details_page'
 import { MarketHomeContainer } from '../../pages/market_sections/market_home_container'
 import { MarketWizardCreatorContainer } from '../../pages/market_sections/market_wizard_creator_container'
 import { MainScroll, MainWrapper, WrongNetworkMessage } from '../common'
@@ -36,6 +37,7 @@ export const Main: React.FC = () => {
                   <Redirect to="/liquidity" />
                 </Route>
                 <Route component={SettingsViewContainer} exact path="/settings" />
+                <Route component={ProposalDetailsPage} exact path="/proposal-details-page" />
                 <Route
                   component={MarketHomeContainer}
                   path={['/24h-volume', '/volume', '/newest', '/ending', '/liquidity']}
--- app/src/pages/guild/proposal_details_page.tsx
+++ app/src/pages/guild/proposal_details_page.tsx
@@ -1,12 +1,39 @@
 import React, { useState } from 'react'
 
+import { ProposalDetailsView } from '../../components/guild/proposal_details_view'
+
 interface Props {
-  id: number
+  amount: any
+  apy: any
+  duration: any
+  marketDetails: any
+  scaleValue: any
+  liqudiity: any
+  totalVolume: any
+  volume: any
+  closingDate: any
+  closingIn: any
+  apyTwo: any
+  verified: any
 }
 
-export const ProposalDetailsPage: React.FC<Props> = props => {
+export const ProposalDetailsPage = () => {
   const [someState, setState] = useState('separation of concerns')
   //logic
-
-  return <div>{someState}</div>
+  const dummyDataPassed = {
+    amount: '500.00 OMN',
+    apy: '360%',
+    duration: '32 days',
+    marketDetails:
+      'What will the June 2021 CME/Globex S&P500 e-mini terminate at? https://www.cmegroup.com/trading/equity-index/us-index/e-mini-sandp500_quotes_globex.html',
+    scaleValue: '90.54',
+    liqudiity: '15,000.00 DAI',
+    totalVolume: '4540.00 DAI',
+    volume: '120.00 DAI',
+    closingDate: '12th January 2021 at 00:00 UTC',
+    closingIn: '32 days',
+    apyTwo: '24.53%',
+    verified: true,
+  }
+  return <ProposalDetailsView {...dummyDataPassed} />
 }
