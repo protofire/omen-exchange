d875420ad1f1f844e62eb83cfe963cea3a64df30
Francesco Vauban <57708955+Frankaus@users.noreply.github.com>
2021-08-18 14:31:22 +0200

    Fix: Font-size and margin on Settings and broken RPC (#2182)
    * Fix: Changed font-size and removed margin-left as per design spec
    
    * Fix: Specified font-family for both RPC text and button_round
    
    * Fix: Specified Roboto font everywhere
    
    * Fix: Set fixed height to 40px
    
    Co-authored-by: Violet <33226956+Mi-Lan@users.noreply.github.com>
    Co-authored-by: Thomas Pulber <corkus@pm.me>

--- app/src/components/button/button_round/index.tsx
+++ app/src/components/button/button_round/index.tsx
@@ -26,6 +26,7 @@ const Wrapper = styled.button<{ active?: boolean }>`
   justify-content: center;
   outline: none;
   padding: ${props => props.theme.buttonRound.padding};
+  font-family: Roboto;
   font-size: ${props => props.theme.buttonRound.fontSize};
   line-height: ${props => props.theme.buttonRound.lineHeight};
   color: ${({ theme }) => theme.colors.textColorDark};
