55de0ff53119650ca00968dd6bd89da8db429617
Violet <milanrasovic.f@gmail.com>
2023-08-02 16:35:56 +0200

    fix: unused code

--- app/src/hooks/useRealityLink.tsx
+++ app/src/hooks/useRealityLink.tsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react'
 
 export const useRealityLink = (relay?: boolean): string => {
-  const [connected, setConnected] = useState<boolean>()
+  // const [connected, setConnected] = useState<boolean>()
   const [chainId, setChainId] = useState<Maybe<string>>(null)
   const windowObj: any = window
 
   useEffect(() => {
     const fetchUserAccounts = async () => {
-      const account = await windowObj.ethereum.request({ method: 'eth_accounts' })
+      // const account = await windowObj.ethereum.request({ method: 'eth_accounts' })
       setChainId(windowObj.ethereum.chainId)
 
-      setConnected(account.length !== 0 && chainId === '0x1')
+      // setConnected(account.length !== 0 && chainId === '0x1')
 
-      windowObj.ethereum.on('accountsChanged', (accountsEvent: string[]) => {
-        setConnected(accountsEvent.length !== 0)
-      })
-      windowObj.ethereum.on('chainChanged', (chaindIdEvent: string) => {
-        setConnected(account.length !== 0 && chaindIdEvent === '0x1')
-      })
+      // windowObj.ethereum.on('accountsChanged', (accountsEvent: string[]) => {
+      //   setConnected(accountsEvent.length !== 0)
+      // })
+      // windowObj.ethereum.on('chainChanged', (chaindIdEvent: string) => {
+      //   setConnected(account.length !== 0 && chaindIdEvent === '0x1')
+      // })
     }
     if (windowObj.ethereum) {
       fetchUserAccounts()
